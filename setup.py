27b6d8d76c55820f149945f5efa9d20224b04ec1
JCZuurmond <>
2019-03-01 20:10:37 +0100

    cleaner deps

--- setup.py
+++ setup.py
@@ -2,9 +2,8 @@ from setuptools import setup, find_packages
 
 
 base_packages = ["numpy>=1.15.4", "scipy>=1.2.0", "scikit-learn>=0.20.2",
-                 "pandas>=0.23.4", "matplotlib>=3.0.2", "torch>=1.0.0",
-                 "plotnine>=0.5.1", "pytest==4.0.2", "jupyter==1.0.0",
-                 "jupyterlab==0.35.4", "flake8==3.6.0"]
+                 "pandas>=0.23.4", "matplotlib>=3.0.2", "pytest==4.0.2",
+                 "jupyter==1.0.0", "jupyterlab==0.35.4", "flake8==3.6.0"]
 
 setup(
     name="scikit-lego",
