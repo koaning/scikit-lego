94641135103c34f6b1fa16e7788cb06e9fb2b9be
MBrouns <>
2019-03-17 16:38:23 +0100

    Add EstimatorTransformer and minor api touchups (#33)
    * rename `n_bins` in `RandomAdder` to noise so that the `get_params` will return a dictionary containing the same name as the constructor as this is what sklearn expects
    
    * add test on generic api of all estimators and  transformers
    
    * add estimatortransformer and let randomadder adhere to sklearn api
    
    * make RandomRegressor adhere to sklearn conentions more

--- sklego/dummy.py
+++ sklego/dummy.py
@@ -1,35 +1,44 @@
 import numpy as np
 from sklearn.base import BaseEstimator, RegressorMixin
+from sklearn.utils import check_X_y
+from sklearn.utils.validation import check_is_fitted, check_array, check_random_state, FLOAT_DTYPES
 
 
 class RandomRegressor(BaseEstimator, RegressorMixin):
-    def __init__(self, strategy="uniform", seed=42):
+    def __init__(self, strategy="uniform", random_state=None):
         """
         :param strategy: One of 'uniform', 'normal' or 'gmm'.
         """
         self.allowed_strategies = ("uniform", "normal")
         if strategy not in self.allowed_strategies:
             raise ValueError(f"strategy {strategy} is not in {self.allowed_strategies}")
-        self.seed = seed
+        self.random_state = random_state
         self.strategy = strategy
-        self.min_ = None
-        self.max_ = None
-        self.mu_ = None
-        self.sigma_ = None
+
         self.gmm = None
 
     def fit(self, X, y):
+        X, y = check_X_y(X, y, estimator=self, dtype=FLOAT_DTYPES)
+        self.dim_ = X.shape[1]
+
         self.min_ = np.min(y)
         self.max_ = np.max(y)
         self.mu_ = np.mean(y)
         self.sigma_ = np.std(y)
+
         return self
 
     def predict(self, X):
-        np.random.seed(self.seed)
+        rs = check_random_state(self.random_state)
+        check_is_fitted(self, ['dim_', 'min_', 'max_', 'mu_', 'sigma_'])
+
+        X = check_array(X, estimator=self, dtype=FLOAT_DTYPES)
+        if X.shape[1] != self.dim_:
+            raise ValueError(f'Unexpected input dimension {X.shape[1]}, expected {self.dim_}')
         if self.strategy not in self.allowed_strategies:
             raise ValueError(f"strategy {self.strategy} is not in {self.allowed_strategies}")
+
         if self.strategy == 'normal':
-            return np.random.normal(self.mu_, self.sigma_, X.shape[0])
+            return rs.normal(self.mu_, self.sigma_, X.shape[0])
         if self.strategy == 'uniform':
-            return np.random.uniform(self.min_, self.max_, X.shape[0])
+            return rs.uniform(self.min_, self.max_, X.shape[0])
--- tests/test_transformers/test_randomadder.py
+++ tests/test_transformers/test_randomadder.py
@@ -4,12 +4,10 @@ from sklego.transformers import RandomAdder
 
 
 def test_dtype_regression(random_xy_dataset_regr):
-    for transformer in [RandomAdder]:
-        X, y = random_xy_dataset_regr
-        assert transformer().fit(X, y).transform(X).dtype == np.float
+    X, y = random_xy_dataset_regr
+    assert RandomAdder().fit(X, y).transform(X).dtype == np.float
 
 
 def test_dtype_classification(random_xy_dataset_clf):
-    for transformer in [RandomAdder]:
-        X, y = random_xy_dataset_clf
-        assert transformer().fit(X, y).transform(X).dtype == np.float
+    X, y = random_xy_dataset_clf
+    assert RandomAdder().fit(X, y).transform(X).dtype == np.float
