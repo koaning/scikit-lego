09dac1bbac3f96f35827edc0fdef653b09931313
MBrouns <matthijs.brouns@gmail.com>
2019-03-17 11:42:38 +0100

    make RandomRegressor adhere to sklearn conentions more

--- sklego/transformers.py
+++ sklego/transformers.py
@@ -9,21 +9,22 @@ class RandomAdder(TransformerMixin, BaseEstimator):
         self.random_state = random_state
 
     def fit(self, X, y):
-        self.random_state_ = check_random_state(self.random_state)
+
         X, y = check_X_y(X, y, estimator=self, dtype=FLOAT_DTYPES)
         self.dim_ = X.shape[1]
 
         return self
 
     def transform(self, X):
-        check_is_fitted(self, ['random_state_', 'dim_'])
+        rs = check_random_state(self.random_state)
+        check_is_fitted(self, ['dim_'])
 
         X = check_array(X, estimator=self, dtype=FLOAT_DTYPES)
 
         if X.shape[1] != self.dim_:
             raise ValueError(f'Unexpected input dimension {X.shape[1]}, expected {self.dim_}')
 
-        return X + self.random_state_.normal(0, self.noise, size=X.shape)
+        return X + rs.normal(0, self.noise, size=X.shape)
 
 
 class EstimatorTransformer(TransformerMixin, MetaEstimatorMixin, BaseEstimator):
--- tests/test_api.py
+++ tests/test_api.py
@@ -4,6 +4,7 @@ import pytest
 from sklearn.linear_model import LinearRegression
 from sklearn.utils import estimator_checks
 
+from sklego.dummy import RandomRegressor
 from sklego.transformers import EstimatorTransformer, RandomAdder
 from tests.conftest import id_func
 
@@ -11,6 +12,7 @@ from tests.conftest import id_func
 @pytest.mark.parametrize("estimator", [
     RandomAdder(),
     EstimatorTransformer(LinearRegression()),
+    RandomRegressor(),
 ], ids=id_func)
 def test_check_estimator(estimator, monkeypatch):
     """Uses the sklearn `check_estimator` method to verify our custom estimators"""
@@ -21,6 +23,10 @@ def test_check_estimator(estimator, monkeypatch):
     skips = defaultdict(list, {
         RandomAdder: [
             'check_methods_subset_invariance',  # Since we add noise, the method is not invariant on a subset
+        ],
+        RandomRegressor: [
+            'check_methods_subset_invariance',  # Since we add noise, the method is not invariant on a subset
+            'check_regressors_train',  # RandomRegressors score is not always greater than 0.5 due to randomness
         ]
     })
 
