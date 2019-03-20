1b804f96de57f8403dfa386caf06f13bb65609cf
Menziess <stefan_schenk@hotmail.com>
2019-03-20 16:37:10 +0100

    Update: made functions private, checks key error.

--- sklego/pandas.py
+++ sklego/pandas.py
@@ -1,14 +1,19 @@
 import pandas as pd
 
 
-def negate_lags(lags):
+def _negate_lags(lags):
     """
-    Helper function, negates an int or list of ints.
+    Helper function, negates an int or iterable of ints.
 
-    >>> negate_lags(3)
+    :param lags: iterable of ints or int.
+    :returns: iterable of ints that are negated.
+
+    :Example:
+
+    >>> _negate_lags(3)
     [-3]
 
-    >>> negate_lags([3])
+    >>> _negate_lags([3])
     [-3]
     """
 
@@ -23,15 +28,20 @@ def negate_lags(lags):
         return list(map(negate, lags))
 
 
-def expand_columns(cols):
+def _expand_columns(cols):
     """
     Helper function, returns list of one or more columns
     from column or list of columns.
 
-    >>> expand_columns('test')
+    :param cols: an iterable of column names or a single column name string.
+    :returns: an iterable of column names.
+
+    :Example:
+
+    >>> _expand_columns('test')
     ['test']
 
-    >>> expand_columns(['test1', 'test2'])
+    >>> _expand_columns(['test1', 'test2'])
     ['test1', 'test2']
     """
 
@@ -44,7 +54,14 @@ def expand_columns(cols):
 
 def add_lag(X, cols, lags):
     """
-    Append a shifted column.
+    Appends lag column(s).
+
+    :param X: array-like, shape=(n_columns, n_samples,) training data.
+    :param cols: an iterable of column names or a single column name string.
+    :param lags: iterable of ints or int.
+    :returns: ``pd.DataFrame`` with only the selected columns
+
+    :Example:
 
     >>> import pandas as pd
     >>> df = pd.DataFrame([[1, 2, 3],
@@ -63,31 +80,44 @@ def add_lag(X, cols, lags):
     1  1  2  3  7.0  8.0
     """
 
-    lags = negate_lags(lags)
+    lags = _negate_lags(lags)
 
-    # Check the X instance
+    # Check the X instance of pandas DataFrame
     if isinstance(X, pd.core.frame.DataFrame):
-        return add_lagged_pandas_columns(X, cols, lags)
+        return _add_lagged_pandas_columns(X, cols, lags)
 
-    # If X can not be handled
-    raise NotImplementedError("Please pass a pandas DataFrame")
+    raise ValueError("Please pass a pandas DataFrame")
 
 
-def add_lagged_pandas_column(df, col, lag):
+def _add_lagged_pandas_columns(df, cols, lags):
     """
-    Return generator of each lag for col.
-    """
-    return df[col].shift(lag).rename(col + str(lag))
+    Append a lag columns, removes all NA values.
 
+    :param df: the input ``pd.DataFrame``.
+    :param cols: an iterable of column names, or a single column name.
+    :returns: ``pd.DataFrame`` with the concatenated lagged columns.
 
-def add_lagged_pandas_columns(df, cols, lags):
-    """
-    Append a shifted column.
+    :Example:
+
+    >>> df = pd.DataFrame([[1, 2, 3],
+    ...                    [4, 5, 6],
+    ...                    [7, 8, 9]],
+    ...                    columns=['a', 'b', 'c'],
+    ...                    index=[1, 2, 3])
+
+    >>> _add_lagged_pandas_columns(df, ['last_name'], 1)
+    Traceback (most recent call last):
+        ...
+    KeyError
     """
 
-    cols = expand_columns(cols)
+    cols = _expand_columns(cols)
+
+    if not all([col in df.columns.values for col in cols]):
+        raise KeyError
+
     combos = [
-        add_lagged_pandas_column(df, col, lag)
+        df[col].shift(lag).rename(col + str(lag))
         for col in cols
         for lag in lags
     ]
