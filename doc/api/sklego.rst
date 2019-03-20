ef5037d6e1d80c33241ab6df14764c4f16de40ec
vincent <vincentwarmerdam@gmail.com>
2019-03-20 06:50:28 +0100

    v1 of documentation seems to live

---
+++ doc/api/sklego.rst
@@ -0,0 +1,38 @@
+sklego package
+==============
+
+Submodules
+----------
+
+sklego.common module
+--------------------
+
+.. automodule:: sklego.common
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+sklego.dummy module
+-------------------
+
+.. automodule:: sklego.dummy
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+sklego.transformers module
+--------------------------
+
+.. automodule:: sklego.transformers
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+
+Module contents
+---------------
+
+.. automodule:: sklego
+    :members:
+    :undoc-members:
+    :show-inheritance:
--- sklego/dummy.py
+++ sklego/dummy.py
@@ -3,10 +3,15 @@ from sklearn.base import BaseEstimator, RegressorMixin
 
 
 class RandomRegressor(BaseEstimator, RegressorMixin):
+    """
+    A RandomRegressor makes random predictions only based on the "y"
+    value that is seen. The goal is that such a regressor can be used
+    for benchmarking. It should be easily beatable.
+
+    :param str strategy: how we want to select random values, can be "uniform" or "normal"
+    :param int seed: the seed value, default: 42
+    """
     def __init__(self, strategy="uniform", seed=42):
-        """
-        :param strategy: One of 'uniform', 'normal' or 'gmm'.
-        """
         self.allowed_strategies = ("uniform", "normal")
         if strategy not in self.allowed_strategies:
             raise ValueError(f"strategy {strategy} is not in {self.allowed_strategies}")
@@ -18,7 +23,14 @@ class RandomRegressor(BaseEstimator, RegressorMixin):
         self.sigma_ = None
         self.gmm = None
 
-    def fit(self, X, y):
+    def fit(self, X: np.array, y: np.array) -> "RandomRegressor":
+        """
+        Fit the model using X, y as training data.
+
+        :param X: array-like, shape=(n_columns, n_samples,) training data.
+        :param y: array-like, shape=(n_samples,) training data.
+        :return: Returns an instance of self.
+        """
         self.min_ = np.min(y)
         self.max_ = np.max(y)
         self.mu_ = np.mean(y)
@@ -26,6 +38,12 @@ class RandomRegressor(BaseEstimator, RegressorMixin):
         return self
 
     def predict(self, X):
+        """
+        Predict new data by making random guesses.
+
+        :param X: array-like, shape=(n_columns, n_samples,) training data.
+        :return: array, shape=(n_samples,) the predicted data
+        """
         np.random.seed(self.seed)
         if self.strategy not in self.allowed_strategies:
             raise ValueError(f"strategy {self.strategy} is not in {self.allowed_strategies}")
