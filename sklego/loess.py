5dab7e3ff24162fe11f04b0ea754bff2f5335099
Tomislav Suhina <tsuhina@xccelerated.io>
2019-03-20 15:41:55 +0100

    to revert

--- sklego/loess.py
+++ sklego/loess.py
@@ -4,50 +4,28 @@ from sklearn.linear_model import LinearRegression
 
 
 class LoessSmoother:
-    def __init__(self, model='linear', window=20, step_size=0.2):
-        self.window = window
+    def __init__(self, model='linear', n_points=5):
+        self.n_points = n_points
+        # This is to be adjusted:
         self.model = LinearRegression()
-        self.n_points = 2
-        self.step_size = step_size
 
-    def generate_reg_indexes(self, X):
-        """Return the focal points and the indices of the x values in their accompanying windows"""
-        x_focal = X.min()
-        x_max = X.max()
+    def get_point_sets(self, X):
+        knn = NearestNeighbors(n_neighbors=self.n_points).fit(X)
 
-        while x_focal <= x_max:
-            yield x_focal, np.argwhere((X > (x_focal - self.window)) & (X < (x_focal + self.window)))
-            x_focal += self.step_size
-
-    def get_closest_points(self, X, x_focal):
-        """return the indices of the nearest points in dataset X to datapoint x_focus"""
-        knn = NearestNeighbors(n_neighbors=self.n_points).fit(X.reshape(-1, 1))
-        return knn.kneighbors(x_focal)[1][0]
+        for point in X:
+            yield knn.kneighbors(point.reshape(-1, 1))[1][0]
 
     def fit(self, X, y):
-        sorting_idx = numpy.argsort(X)
-        X = X[sorting_idx]
-        y = y[sorting_idx]
-        points_generator = self.generate_reg_indexes(X)
+        points_generator = self.get_point_sets(X)
         y_focal = np.array([])
-
-        x_focal_base = np.array([])
-        for x_focal, idx_window in points_generator:
-            #             x_focal = X[idx_focal].reshape(-1, 1)
-            x_focal = x_focal.reshape(-1, 1)
-            x_window = X[idx_window].reshape(-1, 1)
-
-            if len(idx_window) < 2:  # Not possible to fit on less than 2 points
-                idx_window = self.get_closest_points(X, x_focal)
-                x_window = X[idx_window].reshape(-1, 1)
-
+        for idx_focal, idx_window in enumerate(points_generator):
+            x_focal = x[idx_focal].reshape(-1, 1)
+            x_window = x[idx_window]
             y_window = y[idx_window]
-
-            y_focal_new = self.model.fit(x_window, y_window).predict(x_focal)
-
+            y_focal_new = self.model.fit(x_window, y_window).predict(x_focal)[0][0]
             y_focal = np.append(y_focal, y_focal_new)
-            x_focal_base = np.append(x_focal_base, x_focal)
-        return y_focal, x_focal_base
+
+        return y_focal
 
 
 def random_x(minimum_val, maximum_val, size):
