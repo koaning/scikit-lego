ab4c529dc71f8a8aaa776ee4997027cb909d76d2
Hoogland, Kay (ITCDDV) - KLM <Kay.Hoogland@klm.com>
2019-03-20 12:32:37 +0100

    ValueErrors for more explicit checking, added none type as default to make them immutable

--- tests/test_transformers/test_pandastypeselector.py
+++ tests/test_transformers/test_pandastypeselector.py
@@ -25,7 +25,7 @@ def test_len_classification(transformer, random_xy_dataset_clf):
 
 
 @pytest.mark.parametrize('include,exclude', [_ for _ in it.combinations(['number', 'datetime', 'timedelta',
-                                                                         'category', 'datetimetz', ''], 2)])
+                                                                         'category', 'datetimetz', None], 2)])
 def test_get_params_str(include, exclude):
     transformer = PandasTypeSelector(include=include, exclude=exclude)
 
@@ -44,3 +44,23 @@ def test_get_params_np(include, exclude):
         'include': include,
         'exclude': exclude
     }
+
+
+def test_value_error_empty(random_xy_dataset_regr):
+    transformer = PandasTypeSelector(exclude=['number'])
+    X, y = random_xy_dataset_regr
+    X = pd.DataFrame(X)
+
+    with pytest.raises(ValueError):
+        transformer.fit(X, y)
+
+
+def test_value_error_inequal(random_xy_dataset_regr):
+    transformer = PandasTypeSelector(include=['number'])
+    X, y = random_xy_dataset_regr
+    X = pd.DataFrame(X)
+    if len(list(X)) > 0:
+        with pytest.raises(ValueError):
+            transformer.fit(X)
+            # Remove column to create error
+            transformer.transform(X.iloc[:, :-1])
