4213b1fa13fce19c4028a985e2cfe6afa86c73a2
vincent d warmerdam <>
2019-03-29 17:40:45 +0100

    readthedocs (#85)
    * added another banner to the readme
    
    * doc fix for common module
    
    * push more changes!

--- sklego/common.py
+++ sklego/common.py
@@ -11,32 +11,33 @@ class TrainOnlyTransformerMixin(TransformerMixin):
     Allows using a separate function for transforming train and test data
 
     Usage:
-    >>> from sklearn.base import BaseEstimator
-    >>> class TrainOnlyTransformer(TrainOnlyTransformerMixin, BaseEstimator):
-    ...     def fit(self, X, y):
-    ...         super().fit(X, y)
-    ...
-    ...     def transform_train(self, X, y=None):
-    ...          return X + np.random.normal(0, 1, size=X.shape)
-    ...
-    >>> X_train, X_test = np.random.randn(100, 4), np.random.randn(100, 4)
-    >>> y_train, y_test = np.random.randn(100), np.random.randn(100)
-    >>>
-    >>> trf = TrainOnlyTransformer()
-    >>> trf.fit(X_train, y_train)
-    >>>
-    >>> assert np.all(trf.transform(X_train) != X_train)
-    >>> assert np.all(trf.transform(X_test) == X_test)
+        >>> from sklearn.base import BaseEstimator
+        >>> class TrainOnlyTransformer(TrainOnlyTransformerMixin, BaseEstimator):
+        ...     def fit(self, X, y):
+        ...         super().fit(X, y)
+        ...
+        ...     def transform_train(self, X, y=None):
+        ...          return X + np.random.normal(0, 1, size=X.shape)
+        ...
+        >>> X_train, X_test = np.random.randn(100, 4), np.random.randn(100, 4)
+        >>> y_train, y_test = np.random.randn(100), np.random.randn(100)
+        >>>
+        >>> trf = TrainOnlyTransformer()
+        >>> trf.fit(X_train, y_train)
+        >>>
+        >>> assert np.all(trf.transform(X_train) != X_train)
+        >>> assert np.all(trf.transform(X_test) == X_test)
 
-    Transformers using this class as a mixin should at a minimum:
-     - call `super().fit` in their fit method
-     - implement `transform_train()`
+    .. warning:: Transformers using this class as a mixin should at a minimum:
 
-    They may also implement `transform_test()`. If it is not implemented,
-    `transform_test` will simply return the untransformed dataframe
+        - call `super().fit` in their fit method
+        - implement `transform_train()`
+
+        They may also implement `transform_test()`. If it is not implemented,
+        `transform_test` will simply return the untransformed dataframe
     """
 
-    HASHERS = {
+    _HASHERS = {
         pd.DataFrame: lambda X: hashlib.sha256(pd.util.hash_pandas_object(X, index=True).values).hexdigest(),
         np.ndarray: lambda X: hash(X.data.tobytes()),
         np.memmap: lambda X: hash(X.data.tobytes()),
@@ -54,7 +55,7 @@ class TrainOnlyTransformerMixin(TransformerMixin):
     def _hash(X):
         """Returns a hash of X based on the type of X. Hashers are defined in TrainOnlyMixin.HASHERS"""
         try:
-            hasher = TrainOnlyTransformerMixin.HASHERS[type(X)]
+            hasher = TrainOnlyTransformerMixin._HASHERS[type(X)]
         except KeyError:
             raise ValueError(f'Unknown datatype {type(X)}, '
                              f'TransformerSelector only supports {TrainOnlyTransformerMixin.HASHERS.keys()}')
