378cf2de043c1d56a888141fefe38d7f19f3f73b
saracarozza <s.carozza@outlook.com>
2019-04-01 16:46:38 +0200

    removed white spaces

--- sklego/preprocessing.py
+++ sklego/preprocessing.py
@@ -173,24 +173,19 @@ class ColumnsCapper(TransformerMixin):
     def transform(self, X):
         if self.cols_names is None:
             self.cols_names = X.columns
-    
         if self.use_quantile:
-
             if type(self.quantile_thresholds) is float:
                 for col in self.cols_names:
                     q = X[col].quantile(self.quantile_thresholds, interpolation='linear')
                     X[col] = X[col].apply(lambda x: x if (x < q) else q)
-
             if type(self.quantile_thresholds) is list:
                 for col, q_t in zip(self.cols_names, self.quantile_thresholds):
                     q = X[col].quantile(q=q_t, axis=1, interpolation='linear')
                     X[col] = X[col].apply(lambda x: x if (x < q) else q)
         else:
-
             if type(self.absolute_thresholds) is float:
                 for col in self.cols_names:
                     X[col] = X[col].apply(lambda x: x if (x < self.absolute_thresholds) else self.absolute_thresholds)
-
             if type(self.absolute_thresholds) is list:
                 for col, threshold in zip(self.cols_names, self.thresholds):
                     X[col] = X[col].apply(lambda x: x if (x < self.threshold) else self.threshold)
