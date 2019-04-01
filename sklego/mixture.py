ca5a3329d7a12e12fdc1db831bcacd134788a4c8
Dan <daniel.timbrell@ing.com>
2019-04-01 11:53:28 +0200

    clean with flake8

--- sklego/mixture.py
+++ sklego/mixture.py
@@ -21,13 +21,13 @@ class GMMClassifier(BaseEstimator, ClassifierMixin):
     def fit(self, X: np.array, y: np.array) -> "GMMClassifier":
         """
         Fit the model using X, y as training data.
-        
-        :param X: array-like, shape=(n_columns, n_samples,) training data.
-        :param y: array-like, shape=(n_samples,) training data.
+
+        :param X: array-like, shape=(n_columns, n_samples, ) training data.
+        :param y: array-like, shape=(n_samples, ) training data.
         :return: Returns an instance of self.
         """
-        if len(X.shape)==1:
-            X = np.expand_dims(X,1)
+        if len(X.shape) == 1:
+            X = np.expand_dims(X, 1)
 
         X, y = check_X_y(X, y, estimator=self, dtype=FLOAT_DTYPES)
         _check_gmm_keywords(self.gmm_kwargs)
@@ -57,8 +57,8 @@ class GMMOutlierDetector(BaseEstimator):
     a density is trained we can evaluate the likelihood scores to see if
     it is deemed likely. By giving a threshold this model might then label
     outliers if their likelihood score is too low.
-    
-    :param threshold: the limit at which the model thinks an outlier appears, must be between (0,1)
+
+    :param threshold: the limit at which the model thinks an outlier appears, must be between (0, 1)
     :param gmm_kwargs: features that are passed to the `GaussianMixture` from sklearn
     """
     def __init__(self, threshold=0.99, **gmm_kwargs):
@@ -68,15 +68,15 @@ class GMMOutlierDetector(BaseEstimator):
     def fit(self, X: np.array, y=None) -> "GMMOutlierDetector":
         """
         Fit the model using X, y as training data.
-        
+
         :param X: array-like, shape=(n_columns, n_samples,) training data.
         :param y: ignored but kept in for pipeline support
         :return: Returns an instance of self.
         """
-        
-        if len(X.shape)==1:
-            X = np.expand_dims(X,1)
-            
+
+        if len(X.shape) == 1:
+            X = np.expand_dims(X, 1)
+
         X = check_array(X, estimator=self, dtype=FLOAT_DTYPES)
         if (self.threshold > 1) or (self.threshold < 0):
             raise ValueError(f"Threshold {self.threshold} needs to be 0 < threshold < 1")
@@ -87,8 +87,8 @@ class GMMOutlierDetector(BaseEstimator):
 
     def score_samples(self, X):
 
-        if len(X.shape)==1:
-            X = np.expand_dims(X,1)
+        if len(X.shape) == 1:
+            X = np.expand_dims(X, 1)
 
         return self.gmm_.score_samples(X)
 
@@ -97,10 +97,9 @@ class GMMOutlierDetector(BaseEstimator):
         Predict if a point is an outlier. If the output is 0 then
         the model does not think it is an outlier.
 
-        :param X: array-like, shape=(n_columns, n_samples,) training data.
+        :param X: array-like, shape=(n_columns, n_samples, ) training data.
         :return: array, shape=(n_samples,) the predicted data
         """
         X = check_array(X, estimator=self, dtype=FLOAT_DTYPES)
         check_is_fitted(self, ['gmm_', 'likelihood_threshold_'])
         return (self.score_samples(X) < self.likelihood_threshold_).astype(np.int)
-  
