bf2344972922c77715eabd225b309d9d8544162b
Ana Chaloska <anachaloska@Admins-MacBook-Pro-2.local>
2019-04-01 02:23:25 +0200

    Changes based on feedback comments

--- sklego/ensemble_model.py
+++ sklego/ensemble_model.py
@@ -1,5 +1,8 @@
-from sklearn.linear_model import LogisticRegression
+from sklearn.base import clone
+from sklearn.utils.validation import check_is_fitted
 from sklearn.metrics import confusion_matrix
+
+import numpy as np
 
 class EnsembleModel():
     """
@@ -7,14 +10,13 @@ class EnsembleModel():
     Based on the requirements of issue #23 of koaning/scikit-lego
     """
 
-    def __init__(self, X_test, y_test, estimator=LogisticRegression(), alpha=0.5):
+    def __init__(self, estimator=None, alpha=0.5, X_test=None, y_test=None):
         """
 
+        :param estimator: sklean classifier; (eg. LogisticRegression, Decision Tree etc.)
+        :param alpha: hyperparameter; needs to be optimized to increase classification accuracy
         :param X_test: array of feature values that will be used to test the classifier and create a confusion matrix
         :param y_test: array of target values that will be used to test the classifier and create a confusion matrix
-        :param estimator: sklean classifier;
-                          default one is LogisticRegression, but others can be used too (eg. Decision Tree)
-        :param alpha: hyperparameter; needs to be optimized to increase classification accuracy
         """
         self.estimator = estimator
         self.alpha = alpha
@@ -24,64 +26,79 @@ class EnsembleModel():
     def fit(self, X, y):
         """
 
-        :param X: array of feature values used for training the classifier
-        :param y: array of target values used for training the classifier
-        :return: trained classifier (estimator)
-        """
-        self.fitted_estimator_ = self.estimator.fit(X, y)
+       :param X: array of feature values used for training the classifier
+       :param y: array of target values used for training the classifier
+       :return:
+                - trained classifier (self.orig_estimator)
+                - confusion matrix populated with the probabilities of each real value vs model prediction value
+                  of the target variable (self.confusion_matrix)
+                - updated prediction probabilities of each target value made by the trained classifier
+                  (self.prob_matrix)
+       """
+        self.estimator_ = clone(self.estimator)
+        self.orig_estimator_ = self.estimator_.fit(X, y)
+
+        y_pred = self.orig_predict_(self.X_test)
+        self.confusion_matrix = confusion_matrix(self.y_test, y_pred)
+
+        cfm = self.confusion_matrix.T
+        self.prob_matrix = (cfm.T/cfm.sum(axis=1)).T
 
         return self
 
-    def predict(self, X):
+    def orig_predict_(self, X):
         """
 
         :param X: array of feature values upon which a prediction is to be made
-        :return: prediction of the target value made by the trained classfifier
+        :return: prediction of the target value made by the original trained classifier
         """
-        return self.fitted_estimator_.predict(X)
+        return self.orig_estimator_.predict(X)
 
-    def predict_proba(self, X):
+    def orig_predict_proba_(self, X):
         """
 
         :param X: array of feature values upon which a prediction is to be made
-        :return: prediction probabilities of each of the target value made by the trained classifier
+        :return: prediction probabilities of each of the target value made by the original trained classifier
         """
-        return self.fitted_estimator_.predict_proba(X)
+        return self.orig_estimator_.predict_proba(X)
 
-    def confusion_matrix(self):
-        """
-
-        :return: Confusion matrix of the trained classifier based on test data
-        """
-        y_pred = self.predict(self.X_test)
-        return confusion_matrix(self.y_test, y_pred)
-
-    def prob_matrix(self):
+    def p(self, X):
         """
 
-        :return: Confusion matrix populated with the probabilities of each real value vs model prediction value
-                 of the target variable
+        :param X: array of feature values upon which a prediction is to be made
+        :return: original prediction probabilities of each target value made by the trained classifier
         """
-        cfm = self.confusion_matrix().T
-        return (cfm.T / cfm.sum(axis=1)).T
+        p = self.orig_predict_proba_(X)
+        return p
 
     def p_star(self, X):
         """
 
         :param X: array of feature values upon which a prediction is to be made
-        :return: Updated prediction probabilities of each of the target value made by the trained classifier
+        :return: updated prediction probabilities of each target value made by the trained classifier
         """
-        p = self.predict_proba(X)
-        prob_matrix = self.prob_matrix()
-        return p @ prob_matrix
+        p = self.p(X)
+        return  p @ self.prob_matrix
 
-    def make_prediction(self, X):
+    def predict_proba(self, X):
         """
 
         :param X: array of feature values upon which a prediction is to be made
-        :return: Final prediction; based on the initial prediction, modified prediction,
-                 and the (recommendably tuned) alpha parameter
+        :return: prediction probabilities of each of the target value made by the updated trained classifier;
+                 based on the original prediction, updated prediction, and the (recommendably tuned) alpha parameter
         """
-        p = self.predict_proba(X)
+        check_is_fitted(self, 'estimator_')
+        p = self.p(X)
         p_star = self.p_star(X)
-        return (1 - self.alpha) * p + self.alpha * p_star
+        return (1-self.alpha)*p + self.alpha*p_star
+
+    def predict(self, X):
+        """
+
+        :param X: array of feature values upon which a prediction is to be made
+        :return: prediction of the target value made by the updated trained classifier;
+                 based on the original prediction, updated prediction, and the (recommendably tuned) alpha parameter
+        """
+        predict_proba = self.predict_proba(X)
+        prediction = np.argmax(predict_proba, axis=1)
+        return prediction
--- tests/test_estimators/test_ensemble_model.py
+++ tests/test_estimators/test_ensemble_model.py
@@ -1,6 +1,7 @@
 from sklego.ensemble_model import EnsembleModel
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 
 # Data
 iris = datasets.load_iris()
@@ -9,24 +10,25 @@ y = iris.target
 X_train, X_test, y_train, y_test = train_test_split(X, y)
 
 # Model
-model = EnsembleModel(X_test = X_test, y_test = y_test)
+model = EnsembleModel(estimator=LogisticRegression(), X_test = X_test, y_test = y_test)
 model.fit(X_train, y_train)
 
+
 def test_alpha():
-    assert model.alpha >= 0
-    assert model.alpha <= 1
-
-
-def test_predict_proba():
-    assert model.predict([X_test[0]]).shape[0] == 1
+   if model.alpha > 1:
+    assert False
 
 
 def test_confusion_matrix():
-    assert model.confusion_matrix().shape[0] == model.confusion_matrix().shape[1]
+    assert model.confusion_matrix.shape[0] == model.confusion_matrix.shape[1]
 
 
-def test_make_prediction():
-    assert model.make_prediction([X_test[0]]).shape[0] == 1
+def test_predict_proba():
+    assert model.predict_proba([X_test[0]]).shape[0] == 1
+
+
+def test_predict():
+    assert model.predict([X_test[0]]).shape[0] == 1
 
 
 
