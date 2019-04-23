83b63ec5ec18b2e8c2dbd6c38183c34fdbfa04fe
vincent <vincentwarmerdam@gmail.com>
2019-04-23 15:10:07 +0200

    WHOLE LOTTA CHANGEZ!

--- sklego/meta.py
+++ sklego/meta.py
@@ -1,8 +1,11 @@
+from collections import defaultdict
+
 import numpy as np
 import pandas as pd
+
 from sklearn import clone
 from sklearn.base import BaseEstimator
-from sklearn.utils.validation import check_is_fitted
+from sklearn.utils.validation import check_is_fitted, check_X_y, check_array
 
 from sklego.common import as_list
 
@@ -13,14 +16,14 @@ class GroupedEstimator(BaseEstimator):
     single column and fits one estimator per such column.
 
     :param estimator: the model/pipeline to be applied per group
-    :param groupby: the column(s) of the matrix/dataframe to select as a grouping parameter set
+    :param groups: the column(s) of the matrix/dataframe to select as a grouping parameter set
     :param use_fallback: weather or not to fall back to a general model in case
     the group parameter is not found during `.predict()`
     """
 
-    def __init__(self, estimator, groupby, use_fallback=True):
+    def __init__(self, estimator, groups, use_fallback=True):
         self.estimator = estimator
-        self.groupby = [str(_) for _ in as_list(groupby)]
+        self.groups = groups
         self.use_fallback = use_fallback
 
     def fit(self, X, y):
@@ -32,27 +35,30 @@ class GroupedEstimator(BaseEstimator):
         :param y: array-like, shape=(n_samples,) training data.
         :return: Returns an instance of self.
         """
-        self.estimators_ = {}
-        self.groups_ = []
-
+        check_X_y(X, y)
+        pred_col = 'the-column-that-i-want-to-predict-but-dont-have-the-name-for'
         if isinstance(X, np.ndarray):
             X = pd.DataFrame(X, columns=[str(_) for _ in range(X.shape[1])])
-        pred_col = 'the-column-that-i-want-to-predict-but-dont-have-the-name-for'
         X = X.assign(**{pred_col: y})
 
-        self.groups_ = X[self.groupby].drop_duplicates().itertuples(index=False)
-        self.groups_ = [tuple(_) for _ in self.groups_]
+        self.groupby_ = [str(_) for _ in as_list(self.groups)]
+        if any([c not in X.columns for c in self.groupby_]):
+            raise ValueError(f"{self.groupby_} not in {X.columns}")
+        self.cols_to_use_ = [_ for _ in X.columns if _ not in self.groupby_ and _ is not pred_col]
+        self.estimators_ = {}
+        self.fallback_ = None
+        if self.use_fallback:
+            subset_x = X[self.cols_to_use_]
+            self.fallback_ = clone(self.estimator).fit(subset_x, y)
 
-        for group, subset in X.groupby(self.groupby):
+        self.groups_ = []
+        self.groups_ = X[self.groupby_].drop_duplicates()
+
+        for group, subset in X.groupby(self.groupby_):
             group_estimator = clone(self.estimator)
-            subset_x = subset.drop(columns=[pred_col]).drop(columns=self.groupby)
-            subset_y = subset[pred_col]
+            subset_x, subset_y = subset[self.cols_to_use_], subset[pred_col]
             group_estimator.fit(subset_x, subset_y)
             self.estimators_[group] = group_estimator
-
-        if self.use_fallback:
-            subset_x = X.drop(columns=[pred_col]).drop(columns=self.groupby)
-            self.fallback_ = clone(self.estimator).fit(subset_x, y)
 
         return self
 
@@ -63,19 +69,27 @@ class GroupedEstimator(BaseEstimator):
         :param X: array-like, shape=(n_columns, n_samples,) training data.
         :return: array, shape=(n_samples,) the predicted data
         """
-        check_is_fitted(self, ['estimators_', 'groups_'])
-        if self.use_fallback:
-            check_is_fitted(self, ['fallback_'])
+        check_array(X)
+        check_is_fitted(self, ['estimators_', 'groups_', 'groupby_', 'cols_to_use_', 'fallback_'])
         if isinstance(X, np.ndarray):
             X = pd.DataFrame(X, columns=[str(_) for _ in range(X.shape[1])])
-        result = []
-        for row in X.itertuples(index=False):
-            group_to_use = tuple([getattr(row, k) for k in self.groupby])
-            data_to_use = [getattr(row, c) for c in X.columns if c not in self.groupby]
-            if group_to_use in self.groups_:
-                result.append(self.estimators_[group_to_use].predict([data_to_use]))
-            else:
-                if not self.use_fallback:
-                    raise ValueError(f"we see group {group_to_use} while use_fallback={self.use_fallback}")
-                result.append(self.fallback_.predict([data_to_use]))
-        return np.array(result).squeeze()
+
+        if any([c not in X.columns for c in self.groupby_]):
+            raise ValueError(f"group columns {self.groupby_} not in {X.columns}")
+        if any([c not in X.columns for c in self.cols_to_use_]):
+            raise ValueError(f"columns to use {self.cols_to_use_} not in {X.columns}")
+
+        try:
+            return (X
+                    .groupby(self.groupby_, as_index=False)
+                    .apply(lambda d: pd.DataFrame(
+                        self.estimators_.get(d.name, self.fallback_).predict(d[self.cols_to_use_]), index=d.index))
+                    .values
+                    .squeeze())
+        except AttributeError:
+            culprits = set(pd.concat([X[self.groupby_].drop_duplicates().assign(new=1),
+                                   self.groups_.assign(new=0)])
+                             .drop_duplicates()
+                             .loc[lambda d: d['new'] == 1]
+                             .itertuples())
+            raise ValueError(f"found a group(s) {culprits} in `.predict` that was not in `.fit`")
--- tests/test_estimators/test_grouped_model.py
+++ tests/test_estimators/test_grouped_model.py
@@ -1,39 +1,55 @@
 import pytest
 import pandas as pd
 from sklearn.linear_model import LinearRegression
+from sklearn.utils import estimator_checks
 
+from sklego.common import flatten
 from sklego.meta import GroupedEstimator
 from sklego.datasets import load_chicken
-from tests.conftest import nonmeta_checks, general_checks, classifier_checks
 
 
 @pytest.mark.parametrize("test_fn", flatten([
-    nonmeta_checks,
-    general_checks,
-    classifier_checks,
-    check_shape_remains_same_classifier
+    estimator_checks.check_fit_score_takes_y,
+    estimator_checks.check_sample_weights_invariance,
+    estimator_checks.check_estimators_empty_data_messages,
+    estimator_checks.check_estimators_nan_inf,
+    estimator_checks.check_estimators_overwrite_params,
+    estimator_checks.check_estimators_pickle,
+    estimator_checks.check_fit2d_predict1d,
+    estimator_checks.check_fit2d_1sample,
+    estimator_checks.check_fit1d,
+    estimator_checks.check_dont_overwrite_parameters,
+    estimator_checks.check_sample_weights_invariance,
+    estimator_checks.check_get_params_invariance,
+    estimator_checks.check_sample_weights_list,
+    estimator_checks.check_sample_weights_pandas_series,
+    estimator_checks.check_set_params,
 ]))
 def test_estimator_checks(test_fn):
-    clf = GroupedEstimator()
-    test_fn(GroupedEstimator.__name__, clf)
+    clf = GroupedEstimator(estimator=LinearRegression(), groups=[0], use_fallback=True)
+    test_fn(GroupedEstimator.__name__ + "_fallback", clf)
+
+    clf = GroupedEstimator(estimator=LinearRegression(), groups=[0], use_fallback=False)
+    test_fn(GroupedEstimator.__name__ + "_nofallback", clf)
+
 
 def test_chickweight_df1_keys():
     df = load_chicken(give_pandas=True)
-    mod = GroupedEstimator(estimator=LinearRegression(), groupby="diet")
+    mod = GroupedEstimator(estimator=LinearRegression(), groups="diet")
     mod.fit(df[['time', 'diet']], df['weight'])
     assert set(mod.estimators_.keys()) == {1, 2, 3, 4}
 
 
 def test_chickweight_df2_keys():
     df = load_chicken(give_pandas=True)
-    mod = GroupedEstimator(estimator=LinearRegression(), groupby="chick")
+    mod = GroupedEstimator(estimator=LinearRegression(), groups="chick")
     mod.fit(df[['time', 'chick']], df['weight'])
     assert set(mod.estimators_.keys()) == set(range(1, 50 + 1))
 
 
 def test_chickweight_can_do_fallback():
     df = load_chicken(give_pandas=True)
-    mod = GroupedEstimator(estimator=LinearRegression(), groupby="diet")
+    mod = GroupedEstimator(estimator=LinearRegression(), groups="diet")
     mod.fit(df[['time', 'diet']], df['weight'])
     assert set(mod.estimators_.keys()) == {1, 2, 3, 4}
     to_predict = pd.DataFrame({"time": [21, 21], "diet": [5, 6]})
@@ -44,7 +60,7 @@ def test_chickweight_can_do_fallback():
 def test_fallback_can_raise_error():
     df = load_chicken(give_pandas=True)
     mod = GroupedEstimator(estimator=LinearRegression(),
-                           groupby="diet",
+                           groups="diet",
                            use_fallback=False)
     mod.fit(df[['time', 'diet']], df['weight'])
     to_predict = pd.DataFrame({"time": [21, 21], "diet": [5, 6]})
@@ -54,7 +70,7 @@ def test_fallback_can_raise_error():
 
 def test_chickweight_np_keys():
     df = load_chicken(give_pandas=True)
-    mod = GroupedEstimator(estimator=LinearRegression(), groupby=[1, 2])
+    mod = GroupedEstimator(estimator=LinearRegression(), groups=[1, 2])
     mod.fit(df[['time', 'chick', 'diet']].values, df['weight'].values)
     # there should still only be 50 groups on this dataset
     assert len(mod.estimators_.keys()) == 50
