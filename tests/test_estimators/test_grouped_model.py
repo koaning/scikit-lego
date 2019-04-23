76b56cd8136ae581db258b59cf63d2dc549d5a23
vincent <vincentwarmerdam@gmail.com>
2019-04-23 13:49:32 +0200

    added docstrings + tests;

--- tests/test_estimators/test_grouped_model.py
+++ tests/test_estimators/test_grouped_model.py
@@ -1,4 +1,7 @@
+import pytest
+import pandas as pd
 from sklearn.linear_model import LinearRegression
+
 from sklego.meta import GroupedEstimator
 from sklego.datasets import load_chicken
 
@@ -15,6 +18,27 @@ def test_chickweight_df2_keys():
     mod = GroupedEstimator(estimator=LinearRegression(), groupby="chick")
     mod.fit(df[['time', 'chick']], df['weight'])
     assert set(mod.estimators_.keys()) == set(range(1, 50 + 1))
+
+
+def test_chickweight_can_do_fallback():
+    df = load_chicken(give_pandas=True)
+    mod = GroupedEstimator(estimator=LinearRegression(), groupby="diet")
+    mod.fit(df[['time', 'diet']], df['weight'])
+    assert set(mod.estimators_.keys()) == {1, 2, 3, 4}
+    to_predict = pd.DataFrame({"time": [21, 21], "diet": [5, 6]})
+    assert mod.predict(to_predict).shape == (2,)
+    assert mod.predict(to_predict)[0] == mod.predict(to_predict)[1]
+
+
+def test_fallback_can_raise_error():
+    df = load_chicken(give_pandas=True)
+    mod = GroupedEstimator(estimator=LinearRegression(),
+                           groupby="diet",
+                           use_fallback=False)
+    mod.fit(df[['time', 'diet']], df['weight'])
+    to_predict = pd.DataFrame({"time": [21, 21], "diet": [5, 6]})
+    with pytest.raises(ValueError):
+        mod.predict(to_predict)
 
 
 def test_chickweight_np_keys():
