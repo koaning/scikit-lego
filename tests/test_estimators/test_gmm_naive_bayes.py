877ee97873960cc76ad8ab2e82a63b0e0bb77ed9
vincent <vincentwarmerdam@gmail.com>
2019-05-23 09:49:29 +0200

    put back all tests. they all passed

--- tests/test_estimators/test_gmm_naive_bayes.py
+++ tests/test_estimators/test_gmm_naive_bayes.py
@@ -1,25 +1,16 @@
 import pytest
 import numpy as np
-from sklearn.utils import estimator_checks
 
 from sklego.common import flatten
 from sklego.naive_bayes import GaussianMixtureNB
 from sklego.testing import check_shape_remains_same_classifier
-from tests.conftest import nonmeta_checks, general_checks
+from tests.conftest import nonmeta_checks, general_checks, classifier_checks
 
 
 @pytest.mark.parametrize("test_fn", flatten([
     nonmeta_checks,
     general_checks,
-    estimator_checks.check_classifier_data_not_an_array,
-    estimator_checks.check_classifiers_classes,
-    estimator_checks.check_estimators_partial_fit_n_features,
-    estimator_checks.check_classifiers_train,
-    estimator_checks.check_supervised_y_2d,
-    estimator_checks.check_supervised_y_no_nan,
-    estimator_checks.check_estimators_unfitted,
-    estimator_checks.check_non_transformer_estimators_n_iter,
-    estimator_checks.check_decision_proba_consistency,
+    classifier_checks,
     check_shape_remains_same_classifier
 ]))
 def test_estimator_checks(test_fn):
