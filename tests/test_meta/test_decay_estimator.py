e2ac5846ffd10dae23f43761390e32475f20295e
vincent <vincentwarmerdam@gmail.com>
2019-05-31 13:40:57 +0200

    added more tests

--- tests/test_meta/test_decay_estimator.py
+++ tests/test_meta/test_decay_estimator.py
@@ -3,11 +3,12 @@ from sklearn.linear_model import LinearRegression, LogisticRegression
 
 from sklego.common import flatten
 from sklego.meta import DecayEstimator
-from tests.conftest import general_checks, classifier_checks, regressor_checks
+from tests.conftest import general_checks, classifier_checks, regressor_checks, nonmeta_checks
 
 
 @pytest.mark.parametrize("test_fn", flatten([
     general_checks,
+    nonmeta_checks,
     regressor_checks
 ]))
 def test_estimator_checks_regression(test_fn):
@@ -17,6 +18,7 @@ def test_estimator_checks_regression(test_fn):
 
 @pytest.mark.parametrize("test_fn", flatten([
     general_checks,
+    nonmeta_checks,
     classifier_checks
 ]))
 def test_estimator_checks_classification(test_fn):
