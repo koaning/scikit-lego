ab4f8621d3f7e642c857bf7d470918dfe20ae9bb
vincent <vincentwarmerdam@gmail.com>
2019-06-13 16:31:30 +0200

    added tests

--- sklego/metrics.py
+++ sklego/metrics.py
@@ -9,9 +9,11 @@ def correlation_score(column):
     :param column: Name of the column (when X is a dataframe)
     or the index of the column (when X is a numpy array).
     :return: Negative correlation between estimator.predict(X) and X[colum]
-    (this way we ensure that larger is better).
+    (in gridsearch, larger is better and we want to typically punish correlation).
     """
     def fairness_metric(estimator, X, y_true=None):
         """Remember: X is the thing going *in* to your pipeline."""
-        return -np.corrcoef(estimator.predict(X), X[column])[1, 0]
+        if isinstance(X, np.ndarray):
+            return -np.abs(np.corrcoef(estimator.predict(X), X[:, column])[1, 0])
+        return -np.abs(np.corrcoef(estimator.predict(X), X[column])[1, 0])
     return fairness_metric
