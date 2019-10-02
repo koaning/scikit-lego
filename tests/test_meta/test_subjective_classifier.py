203cf5638fe9b6712f41e8d2c7fc6d48d4fbe9a9
Jonatan Samoocha <jonatan@fithaxx.com>
2019-10-02 17:40:55 +0200

    Parameterizes test, adds several edge cases
    ... and breaks the posterior computation - it doesn't like all-zero columns in the confusion matrix.

--- tests/test_meta/test_subjective_classifier.py
+++ tests/test_meta/test_subjective_classifier.py
@@ -6,12 +6,20 @@ from sklearn.ensemble import RandomForestClassifier
 from sklego.meta import SubjectiveClassifier
 
 
-def test_posterior_computation():
-    subjective_model = SubjectiveClassifier(RandomForestClassifier, {0: 0.8, 1: 0.2})
+@pytest.mark.parametrize(
+    'classes, prior, cfm, first_class_posterior', [
+        ([0, 1], [0.8, 0.2], [[90, 10], [10, 90]], 0.973),  # numeric classes
+        (['a', 'b'], [0.8, 0.2], [[90, 10], [10, 90]], 0.973),  # char classes
+        ([False, True], [0.8, 0.2], [[90, 10], [10, 90]], 0.973),  # bool classes
+        (['a', 'b', 'c'], [0.8, 0.1, 0.1], [[80, 10, 10], [10, 90, 0], [0, 0, 100]], 0.985),  # n classes
+        ([0, 1], [0.2, 0.8], [[0, 100], [0, 100]], 0)  # failure to predict class by inner estimator
+    ]
+)
+def test_posterior_computation(classes, prior, cfm, first_class_posterior):
+    subjective_model = SubjectiveClassifier(RandomForestClassifier, dict(zip(classes, prior)))
     # pretend fit() was called
-    subjective_model.estimator.classes_ = np.array([0, 1])
-    subjective_model.cfm_ = pd.DataFrame(np.array([[90, 10], [10, 90]]), index=[0, 1], columns=[0, 1])
-    assert 0.973 == pytest.approx(subjective_model._posterior(0, 0), 0.001)
-    assert 0.692 == pytest.approx(subjective_model._posterior(1, 1), 0.001)
-    assert 1 == pytest.approx(subjective_model._posterior(0, 0) + subjective_model._posterior(1, 0), 0.00001)
-    assert 1 == pytest.approx(subjective_model._posterior(1, 1) + subjective_model._posterior(0, 1), 0.00001)
+    subjective_model.estimator.classes_ = np.array(classes)
+    subjective_model.cfm_ = pd.DataFrame(np.array(cfm), index=classes, columns=classes)
+    assert first_class_posterior == pytest.approx(subjective_model._posterior(classes[0], classes[0]), 0.001)
+    for clazz in classes:
+        assert 1 == pytest.approx(sum([subjective_model._posterior(pred, clazz) for pred in classes]), 0.00001)
