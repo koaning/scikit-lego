2d1658a213c55c2c299ccc7820ef135836801931
vincent <vincentwarmerdam@gmail.com>
2019-10-29 21:34:44 +0100

    v1 of this

---
+++ sklego/this.py
@@ -0,0 +1,14 @@
+poem = """
+defer responsibility to the end user,
+this might just empower them.
+all the features and every extension,
+the path towards ruin is paved with good intensions.
+you cannot understand a problem by avoiding it,
+but sometimes it goes away if you ignore them.
+if you don't know the requirements you don't know if they're met,
+if you haven't gotten to where you're going, you aren't there yet.
+the unknown may be ignored,
+but infinity is ever present.
+"""
+
+print(poem)
