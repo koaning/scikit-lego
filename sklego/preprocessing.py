8d3118ec74b856eae9f8bcdd2512820bb22b9be5
Gaurav Sharma <gsharma81999@gmail.com>
2020-01-16 19:57:59 +0530

    update

--- sklego/preprocessing.py
+++ sklego/preprocessing.py
@@ -109,7 +109,8 @@ class PandasTypeSelector(BaseEstimator, TransformerMixin):
                                  f'{X.dtypes}')
         except ValueError as e:
             raise ValueError(f'Columns were not equal during fit and transform') from e
-
+        
+        self._check_X_for_type(X)
         transformed_df = X.select_dtypes(include=self.include, exclude=self.exclude)
 
         return transformed_df
