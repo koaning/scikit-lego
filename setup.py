2fb8fb3d9fccf3fc6e2d0a4c2713115247a19549
vincent <vincentwarmerdam@gmail.com>
2020-01-17 14:52:01 +0100

    black in ci

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ import sklego
 base_packages = ["numpy>=1.16.0", "scipy>=1.2.0", "scikit-learn>=0.20.2",
                  "pandas>=0.23.4", "patsy>=0.5.1", "autograd>=1.2", "cvxpy>=1.0.24", "Deprecated>=1.2.6"]
 docs_packages = ["sphinx>=1.8.5", "sphinx_rtd_theme>=0.4.3", "nbsphinx>=0.4.2"]
-test_packages = ["flake8>=3.6.0", "nbval>=0.9.1", "pytest>=4.0.2", "pytest-cov>=2.6.1", "pytest-mock>=1.6.3"]
+test_packages = ["flake8>=3.6.0", "nbval>=0.9.1", "pytest>=4.0.2", "black>=19.3",
+                 "pytest-cov>=2.6.1", "pytest-mock>=1.6.3", "pre-commit>=1.18.3"]
 util_packages = ["matplotlib>=3.0.2", "plotnine>=0.5.1", "jupyter>=1.0.0", "jupyterlab>=0.35.4"]
 dev_packages = docs_packages + test_packages + util_packages
 
