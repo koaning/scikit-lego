f60caf86653385056b1f6bf73a7e3e77b83c5464
Gaurav Sharma <gsharma81999@gmail.com>
2020-01-20 00:08:29 +0530

    solved issue 261 (#262)
    * removed redundant if
    
    * removed extra blank line
    
    * remove redundancy from PandasTypeSelector
    
    * exception stacktrace
    
    * update
    
    * removed blank line space
    
    * blacked
    
    Co-authored-by: vincent d warmerdam  <vincentwarmerdam@gmail.com>

--- sklego/preprocessing.py
+++ sklego/preprocessing.py
@@ -83,15 +83,12 @@ class PandasTypeSelector(BaseEstimator, TransformerMixin):
         :param y: not used in this class
         """
         self._check_X_for_type(X)
-        self.type_columns_ = list(
-            X.select_dtypes(include=self.include, exclude=self.exclude)
-        )
         self.X_dtypes_ = X.dtypes
         self.feature_names_ = list(
             X.select_dtypes(include=self.include, exclude=self.exclude).columns
         )
 
-        if len(self.type_columns_) == 0:
+        if len(self.feature_names_) == 0:
             raise ValueError(f"Provided type(s) results in empty dateframe")
 
         return self
@@ -104,20 +101,21 @@ class PandasTypeSelector(BaseEstimator, TransformerMixin):
         Transforms pandas dataframe by (de)selecting columns based on their dtype
         :param X: pandas dataframe to select dtypes for
         """
-        check_is_fitted(self, ["type_columns_", "X_dtypes_", "feature_names_"])
+        check_is_fitted(self, ["X_dtypes_", "feature_names_"])
+
+        try:
+            if (self.X_dtypes_ != X.dtypes).any():
+                raise ValueError(
+                    f"Column dtypes were not equal during fit and transform. Fit types: \n"
+                    f"{self.X_dtypes_}\n"
+                    f"transform: \n"
+                    f"{X.dtypes}"
+                )
+        except ValueError as e:
+            raise ValueError(f"Columns were not equal during fit and transform") from e
+
         self._check_X_for_type(X)
-        if (self.X_dtypes_ != X.dtypes).any():
-            raise ValueError(
-                f"Column dtypes were not equal during fit and transform. Fit types: \n"
-                f"{self.X_dtypes_}\n"
-                f"transform: \n"
-                f"{X.dtypes}"
-            )
-
         transformed_df = X.select_dtypes(include=self.include, exclude=self.exclude)
-
-        if set(list(transformed_df)) != set(self.type_columns_):
-            raise ValueError(f"Columns were not equal during fit and transform")
 
         return transformed_df
 
