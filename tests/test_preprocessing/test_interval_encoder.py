95bc4420fc09039391ebc6e59b41d6c60aa6ff0f
Vincent <>
2020-01-26 15:31:08 +0100

    i want it painted black

--- tests/test_preprocessing/test_interval_encoder.py
+++ tests/test_preprocessing/test_interval_encoder.py
@@ -33,6 +33,7 @@ from tests.conftest import transformer_checks, general_checks
 def test_estimator_checks(test_fn):
     test_fn(IntervalEncoder.__name__, IntervalEncoder(n_chunks=2))
 
+
 @pytest.mark.parametrize("chunks", [2, 5, 10])
 def test_obvious_cases(random_xy_dataset_regr, chunks):
     X, y = random_xy_dataset_regr
