71d33b1574b8d3460ef62535f2261792b84772ae
Vincent <vincentwarmerdam@gmail.com>
2020-04-04 20:33:22 +0200

    moar clean!

--- sklego/preprocessing/identitytransformer.py
+++ sklego/preprocessing/identitytransformer.py
@@ -4,10 +4,6 @@ from sklearn.utils import check_array
 from sklearn.utils.validation import check_is_fitted
 
 
-def _identity(X):
-    return X
-
-
 class IdentityTransformer(BaseEstimator, TransformerMixin):
     """
     The identity transformer returns what it is fed. Does not apply anything useful.
@@ -17,14 +13,13 @@ class IdentityTransformer(BaseEstimator, TransformerMixin):
     def fit(self, X, y=None):
         """'Fits' the estimator."""
         X = check_array(X, copy=True, estimator=self)
-        self.fitted_ = True
         self.shape_ = X.shape
         return self
 
     def transform(self, X):
         """'Applies' the estimator."""
         X = check_array(X, copy=True, estimator=self, )
-        check_is_fitted(self, 'fitted_', 'shape_')
+        check_is_fitted(self, 'shape_')
         if X.shape[1] != self.shape_[1]:
             raise ValueError(f"Wrong shape is passed to transform. Trained on {self.shape_[1]} cols got {X.shape[1]}")
         return X
