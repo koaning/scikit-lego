45e51ba35902ad83dbbb154f375d7355d9fef01e
Pim <pimhoeven@gmail.com>
2020-07-16 18:24:37 +0200

    Some improvements to tests and addition of value testing through correlations

--- tests/test_meta/test_grouped_transformer.py
+++ tests/test_meta/test_grouped_transformer.py
@@ -12,12 +12,22 @@ from sklego.meta import GroupedTransformer
 from tests.conftest import transformer_checks, nonmeta_checks, general_checks
 
 
+@pytest.fixture
+def tests_to_skip():
+    return [
+        # Nonsense checks because we always need at least two columns (group and value)
+        "check_fit2d_1feature",
+        "check_fit2d_predict1d",
+    ]
+
+
 @pytest.mark.parametrize(
     "test_fn", flatten([transformer_checks, nonmeta_checks, general_checks])
 )
-def test_estimator_checks(test_fn):
-    trf = GroupedTransformer(StandardScaler())
-    test_fn(GroupedTransformer.__name__, trf)
+def test_estimator_checks(test_fn, tests_to_skip):
+    if test_fn.__name__ not in tests_to_skip:
+        trf = GroupedTransformer(StandardScaler())
+        test_fn(GroupedTransformer.__name__, trf)
 
 
 def test_values(random_xy_dataset_clf):
@@ -37,10 +47,39 @@ def test_values(random_xy_dataset_clf):
 
     assert transformed.shape == X.shape
 
-    df_with_groups = pd.concat([pd.Series(groups, name="G"), pd.DataFrame(transformed)], axis=1)
+    df_with_groups = pd.concat([pd.Series(groups.flatten(), name="G"), pd.DataFrame(transformed)], axis=1)
 
-    assert (df_with_groups.groupby("G").min() == scaling_range[0]).all(None)
-    assert (df_with_groups.groupby("G").max() == scaling_range[1]).all(None)
+    assert np.allclose(df_with_groups.groupby("G").min(), scaling_range[0])
+    assert np.allclose(df_with_groups.groupby("G").max(), scaling_range[1])
+
+
+def test_group_correlation_standardscaler(random_xy_dataset_clf):
+    X, y = random_xy_dataset_clf
+    X, y = check_X_y(X, y)
+
+    # Make sure all groups are present
+    groups = np.repeat([0, 1], repeats=(X.shape[0] + 1) // 2)[:X.shape[0], np.newaxis]
+    X_with_groups = np.concatenate([groups, X], axis=1)
+
+    # Some weird interval to make sure we test the right values
+    scaling_range = (13, 42)
+
+    trf = MinMaxScaler(scaling_range)
+    transformer = GroupedTransformer(clone(trf), groups=0)
+    transformed = transformer.fit(X_with_groups, y).transform(X_with_groups)
+
+    # For each column, check that all grouped correlations are 1 (because MinMaxScaler scales linear)
+    for col in range(X.shape[1]):
+        assert (
+            pd.concat([
+                pd.Series(groups.flatten(), name="group"),
+                pd.Series(X[:, col], name="original"),
+                pd.Series(transformed[:, col], name="transformed"),
+            ], axis=1)
+            .groupby("group")
+            .corr()
+            .pipe(np.allclose, 1)
+        )
 
 
 def test_set_params():
@@ -57,9 +96,11 @@ def test_get_params():
 
     assert transformer.get_params() == {
         "transformer__with_mean": True,
-        "transformer__with_std": True,
+        "transformer__with_std": False,
+        "transformer__copy": True,
         "transformer": trf,
-        "predict_func": "predict",
+        "groups": 0,
+        "use_global_model": True,
     }
 
 
@@ -83,7 +124,7 @@ def test_multiple_grouping_columns(random_xy_dataset_clf):
     groups = np.tile(
         # 4x2 array, repeated until it fits
         np.array([[0, 0], [0, 1], [1, 0], [1, 1]]),
-        reps=((X.shape[0] + 1) // 4, 1)
+        reps=((X.shape[0] + 3) // 4, 1)
     )[:X.shape[0], :]
     X_with_groups = np.concatenate([groups, X], axis=1)
 
@@ -91,7 +132,7 @@ def test_multiple_grouping_columns(random_xy_dataset_clf):
     scaling_range = (13, 42)
 
     trf = MinMaxScaler(scaling_range)
-    transformer = GroupedTransformer(clone(trf), groups=0)
+    transformer = GroupedTransformer(clone(trf), groups=(0, 1))
     transformed = transformer.fit(X_with_groups, y).transform(X_with_groups)
 
     assert transformed.shape == X.shape
@@ -101,8 +142,14 @@ def test_multiple_grouping_columns(random_xy_dataset_clf):
         pd.DataFrame(transformed)
     ], axis=1)
 
-    assert (df_with_groups.groupby(["A", "B"]).min() == scaling_range[0]).all(None)
-    assert (df_with_groups.groupby(["A", "B"]).max() == scaling_range[1]).all(None)
+    assert np.allclose(df_with_groups.groupby(["A", "B"]).min(), scaling_range[0])
+
+    # If a group has a single element, it defaults to min, so check wether all maxes are one of the bounds
+    maxes = df_with_groups.groupby(["A", "B"]).max()
+    assert np.all(
+        np.isclose(maxes, scaling_range[1]) | np.isclose(maxes, scaling_range[0])
+        # We have at least some groups larger than 1, so there we should find the max of the range
+    ) and np.any(np.isclose(maxes, scaling_range[1]))
 
 
 def test_missing_groups_transform_global(random_xy_dataset_clf):
@@ -122,14 +169,14 @@ def test_missing_groups_transform_global(random_xy_dataset_clf):
 
     # Array with 2 rows, first column a new group. Remaining top are out of range so should be the range
     X_test = np.concatenate([
-        np.array([[3], [3]]), np.stack([X.min(axis=0) - 1, X.max(axis=0) + 1], axis=0)
+        np.array([[3], [3]]), np.stack([X.min(axis=0), X.max(axis=0)], axis=0)
     ], axis=1)
 
     transformed = transformer.transform(X_test)
 
     # Top row should all be equal to the small value of the range, bottom the other
-    assert (transformed == scaling_range[0]).all(axis=1)[0]
-    assert (transformed == scaling_range[1]).all(axis=1)[1]
+    assert np.allclose(transformed[0, :], scaling_range[0])
+    assert np.allclose(transformed[1, :], scaling_range[1])
 
 
 def test_missing_groups_transform_noglobal(random_xy_dataset_clf):
