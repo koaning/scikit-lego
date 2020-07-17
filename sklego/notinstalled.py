c5c3a9edbd5acad843829f134ef18704795d1205
Pim <pimhoeven@gmail.com>
2020-07-17 11:40:44 +0200

    Docstring and type hints

--- sklego/notinstalled.py
+++ sklego/notinstalled.py
@@ -2,7 +2,21 @@ VERSIONS = {"cvxpy": ">=1.0.24"}
 
 
 class NotInstalledPackage:
-    def __init__(self, package_name, version=None):
+    """
+    Class to gracefully catch ImportErrors for modules and packages that are not installed
+
+    :param package_name (str): Name of the package you want to load
+    :param version (str, Optional): Version of the package
+
+    Usage:
+        >>> try:
+        ...     import thispackagedoesnotexist as package
+        >>> except ImportError:
+        ...     from sklego.notinstalled import NotInstalledPackage
+        ...     package = NotInstalledPackage("thispackagedoesnotexist")
+    """
+
+    def __init__(self, package_name: str, version: str = None):
         self.package_name = package_name
         self.version = version if version else VERSIONS.get(package_name, "")
 
