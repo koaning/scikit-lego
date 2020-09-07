5222eb112f0410083588ad412a987d07be251966
Pim <pimhoeven@gmail.com>
2020-09-07 16:36:09 +0200

    Better error message

--- sklego/notinstalled.py
+++ sklego/notinstalled.py
@@ -1,4 +1,4 @@
-VERSIONS = {"cvxpy": ">=1.0.24"}
+KNOWN_PACKAGES = {"cvxpy": {"version": ">=1.0.24", "extra_name": "cvxpy"}}
 
 
 class NotInstalledPackage:
@@ -18,9 +18,22 @@ class NotInstalledPackage:
 
     def __init__(self, package_name: str, version: str = None):
         self.package_name = package_name
-        self.version = version if version else VERSIONS.get(package_name, "")
+        package_info = KNOWN_PACKAGES.get(package_name, {})
+        self.version = version if version else package_info.get("version", "")
+
+        extra_name = package_info.get("extra_name", None)
+        self.pip_message = (
+            (
+                f"Install extra requirement {package_name} using "
+                + f"`python -m pip install scikit-lego[{extra_name}]` or "
+                + "`python -m pip install scikit-lego[all]`"
+            )
+            if extra_name
+            else ""
+        )
 
     def __getattr__(self, name):
         raise ImportError(
-            f"The package {self.package_name}{self.version} is not installed"
+            f"The package {self.package_name}{self.version} is not installed. "
+            + self.pip_message
         )
