5e42b5a078b4763254be3b96e36d874e9b631e3c
fritshermans <post@fritshermans.nl>
2021-03-25 17:06:56 +0100

    adds unit test for oneclasssvm in test_outlier_classifier.py

--- tests/test_meta/test_outlier_classifier.py
+++ tests/test_meta/test_outlier_classifier.py
@@ -1,6 +1,7 @@
 import pytest
 import numpy as np
 from sklearn.ensemble import IsolationForest
+from sklearn.svm import OneClassSVM
 from sklearn.linear_model import LinearRegression
 from sklearn.neighbors import LocalOutlierFactor
 
@@ -57,6 +58,19 @@ def test_obvious_usecase_isolationforest(dataset):
     outlier_proba = clf_model.predict_proba([[10, 10]])[0]
     assert outlier_proba[0]<0.2
     assert outlier_proba[1]>0.8
+    assert isinstance(clf_model.score(X, y), float)
+
+
+def test_obvious_usecase_oneclass_svm(dataset):
+    X = dataset
+    y = (dataset.max(axis=1) > 3).astype(np.int)
+    outlier_model = OneClassSVM(nu=0.05)
+    clf_model = OutlierClassifier(outlier_model)
+    clf_model.fit(X, y)
+    assert clf_model.predict([[10, 10]]) == np.array([1])
+    assert clf_model.predict([[0, 0]]) == np.array([0])
+    np.testing.assert_array_almost_equal(clf_model.predict_proba([[10, 10]]), np.array([[0, 1]]), decimal=3)
+    np.testing.assert_array_almost_equal(clf_model.predict_proba([[0, 0]]), np.array([[1, 0]]), decimal=3)
     assert isinstance(clf_model.score(X, y), float)
 
 
