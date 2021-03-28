9dd8076a5256bc04977b863dfdce2d22d5d8bca4
fritshermans <post@fritshermans.nl>
2021-03-28 21:28:28 +0200

    implements predict_proba for outlier_classifier (#449)
    * implements predict_proba for outlier_classifier
    
    * adds unit test for predict_proba in outlier_classifier
    
    * makes _predict_proba_sigmoid private
    
    * makes outlier_classifier.py throw an error if ourlier model does not have a decision_function implemented to be used for `predict_proba`
    
    * adds isolationforest unit test for outlier classifier
    
    * adds unit test for oneclasssvm in test_outlier_classifier.py
    
    * adds the name of a outlier model to the exception thrown when the outlier model does not have a `decision_function` method
    
    * changes outlierclassifier predict_proba tests to use pytest parametrization
    
    * refactors unit tests for obvious usecases for outlierclassifier

--- sklego/meta/outlier_classifier.py
+++ sklego/meta/outlier_classifier.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.calibration import _SigmoidCalibration
 from sklego.base import OutlierModel
 from sklearn.utils.validation import check_is_fitted, check_X_y
 
@@ -29,8 +30,16 @@ class OutlierClassifier(BaseEstimator, ClassifierMixin):
         X, y = check_X_y(X, y, estimator=self)
         if not self._is_outlier_model():
             raise ValueError("Passed model does not detect outliers!")
+        if not hasattr(self.model, 'decision_function'):
+            raise ValueError(f'Passed model {self.model} does not have a `decision_function` '
+                             f'method. This is required for `predict_proba` estimation.')
         self.estimator_ = self.model.fit(X, y)
         self.classes_ = np.array([0, 1])
+
+        # fit sigmoid function for `predict_proba`
+        decision_function_scores = self.estimator_.decision_function(X)
+        self._predict_proba_sigmoid = _SigmoidCalibration().fit(decision_function_scores, y)
+
         return self
 
     def predict(self, X):
@@ -45,3 +54,16 @@ class OutlierClassifier(BaseEstimator, ClassifierMixin):
         result = np.zeros(preds.shape)
         result[preds == -1] = 1
         return result
+
+    def predict_proba(self, X):
+        """
+        Predict probability estimates for new data.
+
+        :param X: array-like, shape=(n_columns, n_samples,) input data.
+        :return: array, shape=(n_samples,) the predicted data
+        """
+        check_is_fitted(self, ["estimator_", "classes_"])
+        decision_function_scores = self.estimator_.decision_function(X)
+        probabilities = self._predict_proba_sigmoid.predict(decision_function_scores).reshape(-1, 1)
+        complement = np.ones_like(probabilities) - probabilities
+        return np.hstack((complement, probabilities))
--- tests/test_meta/test_outlier_classifier.py
+++ tests/test_meta/test_outlier_classifier.py
@@ -1,6 +1,9 @@
 import pytest
 import numpy as np
+from sklearn.ensemble import IsolationForest
+from sklearn.svm import OneClassSVM
 from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import LocalOutlierFactor
 
 from sklego.common import flatten
 from sklego.mixture import GMMOutlierDetector
@@ -30,15 +33,17 @@ def dataset():
     return np.random.normal(0, 1, (2000, 2))
 
 
-def test_obvious_usecase_quantile(dataset):
-    mod_quantile = GMMOutlierDetector(threshold=0.999, method="quantile")
-    clf_quantile = OutlierClassifier(mod_quantile)
+@pytest.mark.parametrize('outlier_model', [GMMOutlierDetector(), OneClassSVM(nu=0.05), IsolationForest()])
+def test_obvious_usecase(dataset, outlier_model):
+    outlier_clf = OutlierClassifier(outlier_model)
     X = dataset
     y = (dataset.max(axis=1) > 3).astype(np.int)
-    clf_quantile.fit(X, y)
-    assert clf_quantile.predict([[10, 10]]) == np.array([1])
-    assert clf_quantile.predict([[0, 0]]) == np.array([0])
-    assert isinstance(clf_quantile.score(X, y), float)
+    outlier_clf.fit(X, y)
+    assert outlier_clf.predict([[10, 10]]) == np.array([1])
+    assert outlier_clf.predict([[0, 0]]) == np.array([0])
+    np.testing.assert_array_almost_equal(outlier_clf.predict_proba([[0, 0]]), np.array([[1, 0]]), decimal=3)
+    np.testing.assert_allclose(outlier_clf.predict_proba([[10, 10]]), np.array([[0, 1]]), atol=0.2)
+    assert isinstance(outlier_clf.score(X, y), float)
 
 
 def test_raises_error(dataset):
@@ -48,3 +53,12 @@ def test_raises_error(dataset):
     y = (dataset.max(axis=1) > 3).astype(np.int)
     with pytest.raises(ValueError):
         clf_quantile.fit(X, y)
+
+
+def test_raises_error_no_decision_function(dataset):
+    outlier_model = LocalOutlierFactor()
+    clf_model = OutlierClassifier(outlier_model)
+    X = dataset
+    y = (dataset.max(axis=1) > 3).astype(np.int)
+    with pytest.raises(ValueError):
+        clf_model.fit(X, y)
