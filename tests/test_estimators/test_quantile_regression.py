06ea26a6753f70c561422aec5f63db7b34ae3f8f
Garve <xgarve@gmail.com>
2021-04-29 22:04:50 +0200

    Added QuantileRegression Reduceed LADRegression to QuantileRegression

--- tests/test_estimators/test_quantile_regression.py
+++ tests/test_estimators/test_quantile_regression.py
@@ -1,9 +1,9 @@
-"""Test the LADRegressor."""
+"""Test the QuantileRegression."""
 
 import numpy as np
 import pytest
 
-from sklego.linear_model import LADRegression
+from sklego.linear_model import QuantileRegression
 from sklego.testing import check_shape_remains_same_regressor
 from sklego.common import flatten
 from tests.conftest import general_checks, nonmeta_checks, select_tests, regressor_checks
@@ -27,31 +27,42 @@ def _create_dataset(coefs, intercept, noise=0.0):
 def test_coefs_and_intercept__no_noise(coefs, intercept):
     """Regression problems without noise."""
     X, y = _create_dataset(coefs, intercept)
-    lad = LADRegression()
-    lad.fit(X, y)
+    quant = QuantileRegression()
+    quant.fit(X, y)
 
-    assert lad.score(X, y) > 0.99
+    assert quant.score(X, y) > 0.99
 
 
 @pytest.mark.parametrize("coefs, intercept", test_batch)
 def test_score(coefs, intercept):
     """Tests with noise on an easy problem. A good score should be possible."""
     X, y = _create_dataset(coefs, intercept, noise=1.0)
-    lad = LADRegression()
-    lad.fit(X, y)
+    quant = QuantileRegression()
+    quant.fit(X, y)
 
-    assert lad.score(X, y) > 0.9
+    assert quant.score(X, y) > 0.9
+
+
+@pytest.mark.parametrize("coefs, intercept", test_batch)
+@pytest.mark.parametrize("quantile", np.linspace(0, 1, 7))
+def test_quantile(coefs, intercept, quantile):
+    """Tests with noise on an easy problem. A good score should be possible."""
+    X, y = _create_dataset(coefs, intercept, noise=1.0)
+    quant = QuantileRegression(quantile=quantile)
+    quant.fit(X, y)
+
+    np.testing.assert_almost_equal((quant.predict(X)>=y).mean(), quantile, decimal=2)
 
 
 @pytest.mark.parametrize("coefs, intercept", test_batch)
 def test_coefs_and_intercept__no_noise_positive(coefs, intercept):
     """Test with only positive coefficients."""
     X, y = _create_dataset(coefs, intercept, noise=0.0)
-    lad = LADRegression(positive=True)
-    lad.fit(X, y)
+    quant = QuantileRegression(positive=True)
+    quant.fit(X, y)
 
-    assert all(lad.coef_ >= 0)
-    assert lad.score(X, y) > 0.3
+    assert all(quant.coef_ >= 0)
+    assert quant.score(X, y) > 0.3
 
 
 @pytest.mark.parametrize("coefs, intercept", test_batch)
@@ -59,8 +70,8 @@ def test_coefs_and_intercept__no_noise_regularization(coefs, intercept):
     """Test model with regularization. The size of the coef vector should shrink the larger alpha gets."""
     X, y = _create_dataset(coefs, intercept)
 
-    lads = [LADRegression(alpha=alpha, l1_ratio=0.).fit(X, y) for alpha in range(3)]
-    coef_size = np.array([np.sum(lad.coef_ ** 2) for lad in lads])
+    quants = [QuantileRegression(alpha=alpha, l1_ratio=0.).fit(X, y) for alpha in range(3)]
+    coef_size = np.array([np.sum(quant.coef_ ** 2) for quant in quants])
 
     for i in range(2):
         assert coef_size[i] >= coef_size[i + 1]
@@ -70,23 +81,24 @@ def test_coefs_and_intercept__no_noise_regularization(coefs, intercept):
 def test_fit_intercept_and_copy(coefs, intercept):
     """Test if fit_intercept and copy_X work."""
     X, y = _create_dataset(coefs, intercept, noise=2.0)
-    imb = LADRegression(fit_intercept=False, copy_X=False)
+    imb = QuantileRegression(fit_intercept=False, copy_X=False)
     imb.fit(X, y)
 
     assert imb.intercept_ == 0.0
 
 
 @pytest.mark.parametrize("test_fn", [check_shape_remains_same_regressor])
-def test_lad(test_fn):
-    regr = LADRegression()
-    test_fn(LADRegression.__name__, regr)
+def test_quant(test_fn):
+    regr = QuantileRegression()
+    test_fn(QuantileRegression.__name__, regr)
 
 @pytest.mark.parametrize(
     "regr", [
-         (LADRegression.__name__, LADRegression()),
-         (LADRegression.__name__ + "_positive", LADRegression(positive=True)),
-         (LADRegression.__name__ + "_positive__no_intercept", LADRegression(positive=True, fit_intercept=False)),
-         (LADRegression.__name__ + "_no_intercept", LADRegression(fit_intercept=False))
+         (QuantileRegression.__name__, QuantileRegression()),
+         (QuantileRegression.__name__ + "_positive", QuantileRegression(positive=True)),
+         (QuantileRegression.__name__ + "_positive__no_intercept", QuantileRegression(positive=True, fit_intercept=False)),
+         (QuantileRegression.__name__ + "_no_intercept", QuantileRegression(fit_intercept=False)),
+         (QuantileRegression.__name__ + "_quantile", QuantileRegression(quantile=0.3))
      ]
 )
 @pytest.mark.parametrize(
