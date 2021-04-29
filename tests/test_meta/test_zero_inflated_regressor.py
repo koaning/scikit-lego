6c24fe1950e7d4bbd66e1e392adcae53a644a78f
Robert Kübler <xgarve@gmail.com>
2021-04-29 17:54:13 +0200

    added Quantileregression reduced LADRegression to QuantileRegression

--- tests/test_meta/test_zero_inflated_regressor.py
+++ tests/test_meta/test_zero_inflated_regressor.py
@@ -53,6 +53,22 @@ def test_zero_inflated_example():
     assert zir_score > 0.85
     assert zir_score > et_score
 
+def test_zero_inflated_with_sample_weights_example():
+    from sklearn.model_selection import cross_val_score
+
+    np.random.seed(0)
+    X = np.random.randn(10000, 4)
+    y = ((X[:, 0] > 0) & (X[:, 1] > 0)) * np.abs(X[:, 2] * X[:, 3] ** 2)  # many zeroes here, in about 75% of the cases.
+
+    zir = ZeroInflatedRegressor(
+        classifier=ExtraTreesClassifier(random_state=0),
+        regressor=ExtraTreesRegressor(random_state=0)
+    )
+
+    zir_score = cross_val_score(zir, X, y, fit_params={'sample_weight': np.arange(len(y))}).mean()
+
+    assert zir_score > 0.85
+
 def test_wrong_estimators_exceptions():
     X = np.array([[0.]])
     y = np.array([0.])
