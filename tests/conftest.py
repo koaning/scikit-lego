46ad7b090374618eb2edfdea27a3cf21efc63d2d
vincent d warmerdam <vincentwarmerdam@gmail.com>
2021-06-19 07:45:15 +0200

    Update conftest.py (#474)
    * Update conftest.py
    
    Since these datasets are used all over the place, let's keep 'em small.
    
    * Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,8 +5,8 @@ import pandas as pd
 import pytest
 from sklearn.utils import estimator_checks
 
-n_vals = (10, 100, 5000)
-k_vals = (1, 2, 5)
+n_vals = (10, 500)
+k_vals = (1, 5)
 np_types = (np.int32, np.float32, np.float64)
 
 transformer_checks = (
