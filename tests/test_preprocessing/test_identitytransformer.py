3b4014f02b8bcfb7c5160e81064f7c3db9296cdf
Gleb Levitski <36483986+GLevV@users.noreply.github.com>
2022-09-07 06:42:44 +0000

    Update test_identitytransformer.py

--- tests/test_preprocessing/test_identitytransformer.py
+++ tests/test_preprocessing/test_identitytransformer.py
@@ -30,6 +30,7 @@ def test_same_values(random_xy_dataset_regr):
 
 def test_nan_inf(random_xy_dataset_regr):
     X, y = random_xy_dataset_regr
+    X = X.astype(np.float32)
     X[np.random.ranf(size=X.shape) > 0.9] = np.nan
     X[np.random.ranf(size=X.shape) > 0.9] = -np.inf
     X[np.random.ranf(size=X.shape) > 0.9] = np.inf
