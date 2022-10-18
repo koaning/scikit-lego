328af9a80efc230aaa5f47c324c97e97aede5432
Carlo Lepelaars <info@carlolepelaars.nl>
2022-10-18 21:27:07 +0200

    Use sklearn checks to check get_feature_names_out for `sklego.preprocessing` objects.

--- tests/test_preprocessing/test_general.py
+++ tests/test_preprocessing/test_general.py
@@ -1,6 +1,8 @@
-from sklego.common import TrainOnlyTransformerMixin
+from sklearn.utils.estimator_checks import (check_transformer_get_feature_names_out,
+                                            check_transformer_get_feature_names_out_pandas)
 
 from sklego import preprocessing
+from sklego.common import TrainOnlyTransformerMixin
 
 
 def test_get_feature_names_out_implemented():
@@ -9,4 +11,5 @@ def test_get_feature_names_out_implemented():
         # All preprocessors should have a get_feature_names_out method implemented, except for train only transformers.
         # Availability of get_feature_names_out is standard for all sklearn transformers since version 1.1.
         if not issubclass(class_obj, TrainOnlyTransformerMixin):
-            assert hasattr(class_obj, "get_feature_names_out")
+            assert check_transformer_get_feature_names_out(name=sklego_class, transformer_orig=class_obj)
+            assert check_transformer_get_feature_names_out_pandas(name=sklego_class, transformer_orig=class_obj)
