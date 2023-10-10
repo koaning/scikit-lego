9143bbea1e27c3ad0b13556511be5b13fdb68a44
FBruzzesi <francesco.bruzzesi.93@gmail.com>
2023-10-10 20:00:33 +0200

    make check_X_y optional

--- sklego/meta/decay_estimator.py
+++ sklego/meta/decay_estimator.py
@@ -16,18 +16,27 @@ class DecayEstimator(BaseEstimator):
     This meta estimator will only work for estimators that have a
     "sample_weights" argument in their `.fit()` method.
 
-    The `fit` method computes the weights to pass to the estimator. All the checks are
-    delegated to the wrapped estimator.
+    The `fit` method computes the weights to pass to the estimator.
+
+    .. warning:: By default all the checks on `X` and `y` are delegated to the wrapped estimator.
+
+        To change such behaviour, set `check_X_y` to `True`.
+
+        Remark that if the check is skipped, then `y` should have a `shape` attrbute, which is
+        used to extract the number of samples in training data, and compute the weights.
 
     The DecayEstimator will use exponential decay to weight the parameters.
 
     w_{t-1} = decay * w_{t}
     """
 
-    def __init__(self, model, decay: float = 0.999, decay_func="exponential"):
+    def __init__(
+        self, model, decay: float = 0.999, decay_func="exponential", check_X_y=False
+    ):
         self.model = model
         self.decay = decay
         self.decay_func = decay_func
+        self.check_X_y = check_X_y
 
     def _is_classifier(self):
         return any(
@@ -43,13 +52,16 @@ class DecayEstimator(BaseEstimator):
         """
         Fit the data after adapting the same weight.
 
-        :param X: array-like, shape=(n_columns, n_samples,) training data.
-        :param y: array-like, shape=(n_samples,) training data.
+        :param X: array-like, shape=(n_samples, n_features,) training data.
+        :param y: array-like, shape=(n_samples,) target values.
         :return: Returns an instance of self.
         """
-        X, y = check_X_y(
-            X, y, estimator=self, dtype=FLOAT_DTYPES, ensure_min_features=0
-        )
+
+        if self.check_X_y:
+            X, y = check_X_y(
+                X, y, estimator=self, dtype=FLOAT_DTYPES, ensure_min_features=0
+            )
+
         self.weights_ = np.cumprod(np.ones(y.shape[0]) * self.decay)[::-1]
         self.estimator_ = clone(self.model)
         try:
@@ -67,7 +79,7 @@ class DecayEstimator(BaseEstimator):
         """
         Predict new data.
 
-        :param X: array-like, shape=(n_columns, n_samples,) training data.
+        :param X: array-like, shape=(n_samples, n_features,) data to predict.
         :return: array, shape=(n_samples,) the predicted data
         """
         if self._is_classifier():
