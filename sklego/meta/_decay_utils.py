42bee5e57e5bb1d0cafc86865302d95e0c6c0da2
FBruzzesi <francesco.bruzzesi.93@gmail.com>
2023-11-06 10:50:50 +0100

    integrate decays in estimator

--- sklego/meta/_decay_utils.py
+++ sklego/meta/_decay_utils.py
@@ -5,7 +5,7 @@ class LinearDecay:
     """Class representing a linear decay.
 
     This class generates a linear decay function that maps input data `X`, `y` to a linearly decreasing range from
-    `min_value` to `max_value`. The length and step of the decay is determined by the number of samples in `y`.
+    `max_value` to `min_value`. The length and step of the decay is determined by the number of samples in `y`.
 
     !!! warning
         It is up to the user to sort the dataset appropriately.
@@ -19,7 +19,8 @@ class LinearDecay:
 
     Raises
     ------
-    ValueError : If `min_value` is greater than `max_value`.
+    ValueError
+        If `min_value` is greater than `max_value`.
     """
 
     def __init__(self, min_value=0.0, max_value=1.0):
@@ -27,7 +28,7 @@ class LinearDecay:
         self.max_value = max_value
 
     def __call__(self, X, y):
-        """Compute the linear decay values based on the shape of `y`.
+        """Compute the decay values based on the shape of `y`.
 
         Parameters
         ----------
@@ -39,7 +40,7 @@ class LinearDecay:
         Returns
         -------
         np.ndarray, shape=(n_samples,)
-            The linear decay values.
+            The decay values.
         """
         if self.min_value > self.max_value:
             raise ValueError("`min_value` must be less than or equal to `max_value`")
@@ -64,14 +65,15 @@ class ExponentialDecay:
 
     Raises
     ------
-    ValueError : If `decay_rate` not between 0 and 1.
+    ValueError
+        If `decay_rate` not between 0 and 1.
     """
 
     def __init__(self, decay_rate=0.999):
         self.decay_rate = decay_rate
 
     def __call__(self, X, y):
-        """Compute the exponential decay values based on the shape of `y`.
+        """Compute the decay values based on the shape of `y`.
 
         Parameters
         ----------
@@ -83,7 +85,7 @@ class ExponentialDecay:
         Returns
         -------
         np.ndarray, shape=(n_samples,)
-            The exponential decay values.
+            The decay values.
         """
         if self.decay_rate <= 0 or self.decay_rate >= 1:
             raise ValueError(
@@ -95,7 +97,24 @@ class ExponentialDecay:
 
 
 class StepWiseDecay:
-    """
+    """Class representing a stepwise decay.
+
+    This class generates a stepwise decay function that maps input data `X`, `y` to a decreasing range from
+    `max_value` to `min_value`.
+
+    It is possible to specify one of `n_steps` or `step_size` to determine the behaviour of the decay.
+
+    - If `step_size` is provided, the decay will be split into `n_samples // step_size` steps, each of which will
+        decrease the value by `step_width = (max_value - min_value) / n_steps`.
+    - If `n_steps` is provided, the decay will be split into `n_steps` steps, each of which will decrease the value
+        by `step_width = (max_value - min_value) / n_steps`.
+
+    Each *step* of length *step_size* has constant weight, and then decreases by `step_width` until the minimum value is
+    reached.
+
+    !!! warning
+        It is up to the user to sort the dataset appropriately.
+
     Parameters
     ----------
     n_steps : int | None, default=None
@@ -106,6 +125,18 @@ class StepWiseDecay:
         The minimum value of the decay.
     max_value : float, default=1.
         The maximum value of the decay.
+
+    Raises
+    ------
+    ValueError
+        - If `min_value` is greater than `max_value`.
+        - If no value or both values are provided for `n_steps` or `step_size`.
+        - If `step_size` less than 0 or greater than the number of samples.
+        - If `n_steps` less than 0 or greater than the number of samples.
+
+    TypeError
+        - If `n_steps` is not an integer.
+        - If `step_size` is not an integer.
     """
 
     def __init__(self, n_steps=None, step_size=None, min_value=0.0, max_value=1.0):
@@ -115,6 +146,20 @@ class StepWiseDecay:
         self.max_value = max_value
 
     def __call__(self, X, y):
+        """Compute the decay values based on the shape of `y`.
+
+        Parameters
+        ----------
+        X : array-like, shape=(n_samples, n_features,)
+            Training data. Unused, present for API consistency by convention.
+        y : array-like, shape=(n_samples,)
+            Target values. Used to determine the number of samples in the decay.
+
+        Returns
+        -------
+        np.ndarray, shape=(n_samples,)
+            The decay values.
+        """
         if self.min_value > self.max_value:
             raise ValueError("`min_value` must be less than or equal to `max_value`")
 
@@ -154,9 +199,9 @@ class StepWiseDecay:
                     "`n_steps` must be less than or equal to the number of samples"
                 )
 
+            step_size = n_samples // self.n_steps
             n_steps = self.n_steps
             step_width = (self.max_value - self.min_value) / n_steps
-            step_size = n_samples // self.n_steps
 
         return (
             self.max_value
@@ -165,12 +210,51 @@ class StepWiseDecay:
 
 
 class SigmoidDecay:
+    """Class representing a sigmoid decay.
+
+    This class generates a sigmoid decay function that maps input data `X`, `y` to a non-linearly decreasing range from
+    `max_value` to `min_value`. The steepness of the decay is determined by the `growth_rate` parameter. If not provided
+    this will be set to `10 / n_samples`, which is a good default for most cases.
+
+    !!! warning
+        It is up to the user to sort the dataset appropriately.
+
+    Parameters
+    ----------
+    growth_rate : float | None, default=None
+        The growth rate of the sigmoid function. If not provided this will be set to `10 / n_samples`.
+    min_value : float, default=0.
+        The minimum value of the decay.
+    max_value : float, default=1.
+        The maximum value of the decay.
+
+    Raises
+    ------
+    ValueError
+        - If `min_value` is greater than `max_value`.
+        - If `growth_rate` is not between 0 and 1.
+    """
+
     def __init__(self, growth_rate=None, min_value=0.0, max_value=1.0):
         self.growth_rate = growth_rate
         self.min_value = min_value
         self.max_value = max_value
 
     def __call__(self, X, y):
+        """Compute the decay values based on the shape of `y`.
+
+        Parameters
+        ----------
+        X : array-like, shape=(n_samples, n_features,)
+            Training data. Unused, present for API consistency by convention.
+        y : array-like, shape=(n_samples,)
+            Target values. Used to determine the number of samples in the decay.
+
+        Returns
+        -------
+        np.ndarray, shape=(n_samples,)
+            The decay values.
+        """
         if self.min_value > self.max_value:
             raise ValueError("`min_value` must be less than or equal to `max_value`")
 
