b8b83684aef5cfb54ae6174479379451a6a4f153
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-11-22 13:34:37 +0000

    wip

--- sklego/common.py
+++ sklego/common.py
@@ -1,9 +1,7 @@
 import collections
-import hashlib
 from warnings import warn
 
 import numpy as np
-import pandas as pd
 from sklearn.base import TransformerMixin
 from sklearn.utils.validation import check_array, check_is_fitted, check_X_y
 
