36fff66b55ff80b99a8216774ce8c069a33f42c7
FBruzzesi <francesco.bruzzesi.93@gmail.com>
2023-12-22 17:45:07 +0100

    importlib fix?

--- sklego/__init__.py
+++ sklego/__init__.py
@@ -1,4 +1,9 @@
-from importlib import metadata
+import sys
+
+if sys.version_info >= (3, 8):
+    from importlib import metadata
+else:
+    import importlib_metadata as metadata
 
 __title__ = __name__
 __version__ = metadata.version(__title__)
