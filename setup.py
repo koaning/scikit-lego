eb30b8021b68af1e4908b77ffdd33df6271c65b8
FBruzzesi <francesco.bruzzesi.93@gmail.com>
2023-12-22 18:57:26 +0100

    rollback to setuptools

---
+++ setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+"""
+setup.py is used to distribute scikit-lego as a package using setuptools and twine
+"""
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup()
