e714f727a191c3c1dca3865145af80f0416cfa3d
FBruzzesi <francesco.bruzzesi.93@gmail.com>
2023-12-22 17:40:03 +0100

    importlib >=3.8

--- sklego/__init__.py
+++ sklego/__init__.py
@@ -1,9 +1,4 @@
-import sys
-
-if sys.version_info >= (3, 8):
-    from importlib import metadata
-else:
-    import importlib_metadata as metadata
+from importlib import metadata
 
 __title__ = __name__
 __version__ = metadata.version(__title__)
