a0f0470a0bba919edd30f18867df534494e240fb
fabioscantamburlo <fabioscantamburlo1@gmail.com>
2024-02-12 22:28:44 +0100

    Mkdocs and small fixes

---
+++ docs/api/features-selection.md
@@ -0,0 +1,6 @@
+# Features Selection
+
+:::sklego.feature_selection.mrmr.MaximumRelevanceMinimumRedundancy
+    options:
+        show_root_full_path: true
+        show_root_heading: true
--- sklego/feature_selection/mrmr.py
+++ sklego/feature_selection/mrmr.py
@@ -44,6 +44,7 @@ class MaximumRelevanceMinimumRedundancy(SelectorMixin, BaseEstimator):
     have high relevance to the target variable while minimizing redundancy among the already selected features.
 
     How MRMR works:
+
     1. Compute the relevance of each feature to the target variable: The relevance of a feature is typically
     measured using a metric such as mutual information, correlation coefficient, or another appropriate measure of
     dependence between the feature and the target variable.
@@ -118,7 +119,7 @@ class MaximumRelevanceMinimumRedundancy(SelectorMixin, BaseEstimator):
         self.redundancy_func = redundancy_func
 
     def _get_support_mask(self):
-        """_summary_
+        """SelectorMixin base function to get the selected features mask
 
         Returns
         -------
@@ -174,9 +175,9 @@ class MaximumRelevanceMinimumRedundancy(SelectorMixin, BaseEstimator):
         Raises
         ------
         ValueError
-            if k parameter is not integer type.
-            if k parameter is < n_features_in (X.shape[1])
-            if k parameter < 1
+            if:
+
+                k parameter is not integer type or is < n_features_in (X.shape[1]) or < 1
         """
         self._y_dtype = y.dtype
         relevance = self._get_relevance
@@ -197,30 +198,17 @@ class MaximumRelevanceMinimumRedundancy(SelectorMixin, BaseEstimator):
         elif self.k < 1:
             raise ValueError(f"k parameter mush be >= 1, got {self.k}")
 
-        k = min(self.n_features_in_, self.k)
-
         X, y = check_X_y(X, y)
 
         # computed one time for all features
         rel_score = relevance(X, y)
 
-        for i in range(k):
+        for i in range(self.k):
             red_i = redundancy(X, selected_features, left_features) / (i + 1)
-
-            # print(f"redundnancy i={i} = {red_i}")
-            # print(f"rel_score i = {i} = {rel_score[left_features]}")
-
-            # Fix in case of 0/0 or 0/nan
-            mrmr_score_i = np.nan_to_num(rel_score[left_features] / red_i, np.finfo(float).eps)
-            # mrmr_score_i = rel_score[left_features] / red_i
+            mrmr_score_i = rel_score[left_features] / red_i
             selected_index = np.argmax(mrmr_score_i)
             selected_features += [left_features.pop(selected_index)]
             selected_scores += [mrmr_score_i[selected_index]]
-
-            # print(f"mrmr score i={i} = {mrmr_score_i}")
-            # print(f"Selected index i={i} = {selected_index}")
-            # print(f"Selected score i={i} = {mrmr_score_i[selected_index]}")
-
         self.selected_features_ = np.asarray(selected_features)
         self.scores_ = np.asarray(selected_scores)
         return self
