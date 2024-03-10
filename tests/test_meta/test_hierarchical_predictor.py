a1b7922b523b8d52bf0bbf91234586ba54a01c03
FBruzzesi <francesco.bruzzesi.93@gmail.com>
2024-03-10 12:13:55 +0100

    add test cases

--- tests/test_meta/test_hierarchical_predictor.py
+++ tests/test_meta/test_hierarchical_predictor.py
@@ -1,9 +1,11 @@
 from contextlib import nullcontext as does_not_raise
 
+import numpy as np
 import pandas as pd
 import pytest
 from sklearn import clone
 from sklearn.datasets import make_classification, make_regression
+from sklearn.dummy import DummyClassifier, DummyRegressor
 from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.metrics import accuracy_score, r2_score
 from sklearn.pipeline import Pipeline
@@ -38,12 +40,37 @@ def make_hierarchical_dataset(task):
 
     X = pd.DataFrame(X, columns=[f"x_{i}" for i in range(X.shape[1])]).assign(
         g_0=1,
-        g_1=["A"] * 500 + ["B"] * 500,
-        g_2=["X"] * 250 + ["Y"] * 500 + ["Z"] * 250,
+        g_1=["A"] * (n_samples // 2) + ["B"] * (n_samples // 2),
+        g_2=["X"] * (n_samples // 4) + ["Y"] * (n_samples // 2) + ["Z"] * (n_samples // 4),
     )
     groups = ["g_0", "g_1", "g_2"]
 
     return X, y, groups
+
+
+def make_hierarchical_dummy():
+    df_train = pd.DataFrame(
+        {
+            "g_1": ["A"] * 500 + ["B"] * 500,
+            "g_2": ["X"] * 250 + ["Y"] * 500 + ["Z"] * 250,
+            "target": [0] * 250 + [1] * 500 + [0] * 250,
+        }
+    )
+    # -> will fit the following values: (g_1, g_2) in {(A,X), (A, Y), (B, Y), (B, Z)} and g_1 in {A, B}
+
+    df_pred = pd.DataFrame(
+        [
+            ["A", "X"],
+            ["A", "Y"],
+            ["A", "Z"],  # fallback to estimator for g_1 = A
+            ["B", "X"],  # fallback to estimator for g_1 = B
+            ["B", "Y"],
+            ["B", "Z"],
+            ["C", "X"],  # fallback to global estimator
+        ],
+        columns=["g_1", "g_2"],
+    )
+    return df_train, df_pred
 
 
 @pytest.mark.parametrize(
@@ -142,3 +169,38 @@ def test_shrinkage(meta_cls, base_estimator, task, metric, shrinkage):
     base_model = clone(base_estimator).fit(X.drop(columns=groups), y)
 
     assert metric(y, base_model.predict(X.drop(columns=groups))) <= metric(y, meta_model.predict(X))
+
+
+@pytest.mark.parametrize(
+    "meta_model,method",
+    [
+        (HierarchicalRegressor(DummyRegressor(strategy="mean"), groups=["g_1", "g_2"], check_X=False), "predict"),
+        (
+            HierarchicalClassifier(DummyClassifier(strategy="prior"), groups=["g_1", "g_2"], check_X=False),
+            "predict_proba",
+        ),
+    ],
+)
+@pytest.mark.parametrize(
+    "shrinkage,expected",
+    [
+        # most granual prediction is the class/target itself, fallback to parent or global is the average
+        (None, [0.0, 1.0, 0.5, 0.5, 1.0, 0.0, 0.5]),
+        # prediction is same as global, which is always 0.5
+        (lambda x: np.array([1, 0, 0]), [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5]),
+        # prediction is average of global and most granual i.e. the previous two cases
+        (lambda x: np.array([1, 0, 1]), [0.25, 0.75, 0.5, 0.5, 0.75, 0.25, 0.5]),
+    ],
+)
+def test_expected_output(meta_model, method, shrinkage, expected):
+    df_train, df_test = make_hierarchical_dummy()
+
+    X_train, y_train = df_train[["g_1", "g_2"]], df_train["target"]
+    X_test = df_test[["g_1", "g_2"]]
+
+    meta_model.set_params(shrinkage=shrinkage).fit(X_train, y_train)
+    select_pred = lambda x: x[:, 1] if x.ndim > 1 else x
+
+    y_pred = select_pred(getattr(meta_model, method)(X_test))
+
+    assert np.allclose(expected, y_pred)
