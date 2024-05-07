ea17b3c098cab09e23353ebaab118ba6f83a8706
FBruzzesi <francesco.bruzzesi.93@gmail.com>
2024-05-07 06:45:39 +0200

    test refactor

--- tests/test_preprocessing/test_columndropper.py
+++ tests/test_preprocessing/test_columndropper.py
@@ -1,110 +1,53 @@
+from contextlib import nullcontext as does_not_raise
+
 import pandas as pd
 import polars as pl
 import pytest
 from pandas.testing import assert_frame_equal as pandas_assert_frame_equal
 from polars.testing import assert_frame_equal as polars_assert_frame_equal
-from sklearn.pipeline import make_pipeline
+from sklearn.pipeline import Pipeline, make_pipeline
 
 from sklego.preprocessing import ColumnDropper
 
 
 @pytest.fixture()
-def df():
-    return pd.DataFrame(
-        {
-            "a": [1, 2, 3, 4, 5, 6],
-            "b": [10, 9, 8, 7, 6, 5],
-            "c": ["a", "b", "a", "b", "c", "c"],
-            "d": ["b", "a", "a", "b", "a", "b"],
-            "e": [0, 1, 0, 1, 0, 1],
-        }
-    )
+def data():
+    return {
+        "a": [1, 2, 3, 4, 5, 6],
+        "b": [10, 9, 8, 7, 6, 5],
+        "c": ["a", "b", "a", "b", "c", "c"],
+        "d": ["b", "a", "a", "b", "a", "b"],
+        "e": [0, 1, 0, 1, 0, 1],
+    }
 
 
-@pytest.fixture()
-def df_polars():
-    return pl.DataFrame(
-        {
-            "a": [1, 2, 3, 4, 5, 6],
-            "b": [10, 9, 8, 7, 6, 5],
-            "c": ["a", "b", "a", "b", "c", "c"],
-            "d": ["b", "a", "a", "b", "a", "b"],
-            "e": [0, 1, 0, 1, 0, 1],
-        }
-    )
+@pytest.mark.parametrize(
+    "frame_func, assert_func",
+    [
+        (pd.DataFrame, pandas_assert_frame_equal),
+        (pl.DataFrame, polars_assert_frame_equal),
+    ],
+)
+@pytest.mark.parametrize(
+    "to_drop, context",
+    [
+        (["e"], does_not_raise()),  # one
+        (["a", "b"], does_not_raise()),  # two
+        ([], does_not_raise()),  # none
+        (["a", "b", "c", "d", "e"], pytest.raises(ValueError)),  # all
+        (["f"], pytest.raises(KeyError)),  # not in data
+    ],
+)
+@pytest.mark.parametrize("wrapper", [lambda x: x, make_pipeline])
+def test_drop(data, frame_func, assert_func, to_drop, context, wrapper):
+    sub_data = {k: v for k, v in data.items() if k not in to_drop}
 
+    with context:
+        transformer = wrapper(ColumnDropper(to_drop))
+        result_df = transformer.fit_transform(frame_func(data))
+        expected_df = frame_func(sub_data)
 
-def test_drop_two(df):
-    result_df = ColumnDropper(["a", "b"]).fit_transform(df)
-    expected_df = pd.DataFrame(
-        {
-            "c": ["a", "b", "a", "b", "c", "c"],
-            "d": ["b", "a", "a", "b", "a", "b"],
-            "e": [0, 1, 0, 1, 0, 1],
-        }
-    )
+        assert_func(result_df, expected_df)
 
-    pandas_assert_frame_equal(result_df, expected_df)
-
-
-def test_drop_one(df):
-    result_df = ColumnDropper(["e"]).fit_transform(df)
-    expected_df = pd.DataFrame(
-        {
-            "a": [1, 2, 3, 4, 5, 6],
-            "b": [10, 9, 8, 7, 6, 5],
-            "c": ["a", "b", "a", "b", "c", "c"],
-            "d": ["b", "a", "a", "b", "a", "b"],
-        }
-    )
-
-    pandas_assert_frame_equal(result_df, expected_df)
-
-
-def test_drop_all(df):
-    with pytest.raises(ValueError):
-        ColumnDropper(["a", "b", "c", "d", "e"]).fit_transform(df)
-
-
-def test_drop_none(df):
-    result_df = ColumnDropper([]).fit_transform(df)
-    pandas_assert_frame_equal(result_df, df)
-
-
-def test_drop_not_in_frame(df):
-    with pytest.raises(KeyError):
-        ColumnDropper(["f"]).fit_transform(df)
-
-
-def test_drop_one_in_pipeline(df):
-    pipe = make_pipeline(ColumnDropper(["e"]))
-    result_df = pipe.fit_transform(df)
-    expected_df = pd.DataFrame(
-        {
-            "a": [1, 2, 3, 4, 5, 6],
-            "b": [10, 9, 8, 7, 6, 5],
-            "c": ["a", "b", "a", "b", "c", "c"],
-            "d": ["b", "a", "a", "b", "a", "b"],
-        }
-    )
-
-    pandas_assert_frame_equal(result_df, expected_df)
-
-
-def test_get_feature_names():
-    df = pd.DataFrame({"a": [4, 5, 6], "b": ["4", "5", "6"]})
-    transformer = ColumnDropper("a").fit(df)
-    assert transformer.get_feature_names() == ["b"]
-
-
-def test_drop_two_polars(df_polars):
-    result_df = ColumnDropper(["a", "b"]).fit_transform(df_polars)
-    expected_df = pl.DataFrame(
-        {
-            "c": ["a", "b", "a", "b", "c", "c"],
-            "d": ["b", "a", "a", "b", "a", "b"],
-            "e": [0, 1, 0, 1, 0, 1],
-        }
-    )
-
-    polars_assert_frame_equal(result_df, expected_df)
+        if not isinstance(transformer, Pipeline):
+            assert transformer.get_feature_names() == list(sub_data.keys())
