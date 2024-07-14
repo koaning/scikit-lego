686ea4cff3ff7d01e4c64df70e420299d3bae24f
David Masip <david26694@gmail.com>
2024-07-14 16:56:43 +0200

    format make_classification call
    Co-authored-by: Francesco Bruzzesi <42817048+FBruzzesi@users.noreply.github.com>

--- sklego/linear_model.py
+++ sklego/linear_model.py
@@ -725,7 +725,13 @@ class DemographicParityClassifier(BaseEstimator, LinearClassifierMixin):
     from sklearn.datasets import make_classification
     from sklearn.model_selection import train_test_split
 
-    X, y = make_classification(n_samples=100, n_features=2, n_informative=2, n_redundant=0, n_clusters_per_class=1)
+    X, y = make_classification(
+        n_samples=100,
+        n_features=2,
+        n_informative=2,
+        n_redundant=0,
+        n_clusters_per_class=1,
+    )
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
