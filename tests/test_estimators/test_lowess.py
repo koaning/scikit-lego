6e5f948eb7d044d6d52b53fb91d476d6796b8802
Francesco Bruzzesi <42817048+FBruzzesi@users.noreply.github.com>
2024-11-13 11:10:08 +0100

    chore: improve error message for `ZeroDivisionError` in `LowessRegression` (#708)
    * chore: improve error message for ZeroDivisionError in LowessRegression
    
    * comment for future selves
    
    ---------
    
    Co-authored-by: vincent d warmerdam <vincentwarmerdam@gmail.com>

--- tests/test_estimators/test_lowess.py
+++ tests/test_estimators/test_lowess.py
@@ -1,4 +1,7 @@
+import re
+
 import numpy as np
+import pytest
 from sklearn.utils.estimator_checks import parametrize_with_checks
 
 from sklego.linear_model import LowessRegression
@@ -15,3 +18,18 @@ def test_obvious_usecase():
     y = np.ones(x.shape)
     y_pred = LowessRegression().fit(X, y).predict(X)
     assert np.isclose(y, y_pred).all()
+
+
+def test_custom_error_for_zero_division():
+    x = np.arange(0, 100)
+    X = x.reshape(-1, 1)
+    y = np.ones(x.shape)
+    estimator = LowessRegression(sigma=1e-10).fit(X, y)
+
+    with pytest.raises(
+        ValueError, match=re.escape("Weights, resulting from `np.exp(-(distances**2) / self.sigma)`, are all zero.")
+    ):
+        # Adding an offset, otherwise X to predict would be the same as X in fit method,
+        # leading to weight of 1 for the corresponding value.
+        X_pred = X[:10] + 0.5
+        estimator.predict(X_pred)
