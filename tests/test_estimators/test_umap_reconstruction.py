93060a83fcaebb02ceac20a51fee727903876a2d
FBruzzesi <francesco.bruzzesi.93@gmail.com>
2025-11-11 09:18:15 +0100

    umap

--- tests/test_estimators/test_umap_reconstruction.py
+++ tests/test_estimators/test_umap_reconstruction.py
@@ -18,6 +18,7 @@ def test_sklearn_compatible_estimator(estimator, check):
         # Numba 0.62.0 fails on this check, probably temporary issue
         "check_estimators_dtypes",
         "check_f_contiguous_array_estimator",
+        "check_pipeline_consistency",
     }:
         pytest.skip()
 
